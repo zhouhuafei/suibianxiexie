eeb7d64fcd8399cc3b592a978534cea16e47401d
zhouhuafei <zhouhuafei@shopex.cn>
2016-11-25 10:11:39 +0800

    js

--- app.js
+++ app.js
@@ -10,7 +10,21 @@ var app=express();
 var Routes=require('./routes/routes');//路由
 new Routes({app:app});
 
-var server=app.listen(config.getPort(),function(){//监听
+app.use(function(req,res){//404
+    res.type('text/plain');
+    res.status('404');
+    res.send('404 - Not Found');
+});
+
+app.use(function(err,req,res,next){//500
+    console.log(err.stack);
+    res.type('text/plain');
+    res.status('500');
+    res.send('500 - Server Error');
+});
+
+app.set('port',config.getPort());
+var server=app.listen(app.get('port'),function(){//端口
     var port=server.address().port;
     console.log('访问地址:http://127.0.0.1:'+port);
 });
