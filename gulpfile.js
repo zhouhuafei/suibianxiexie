a40049fd06ff2000914fb59d93a4354027d79bb5
zhouhuafei <1123486116@qq.com>
2016-12-05 22:32:28 +0800

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@
  * Created by zhouhuafei on 16/12/4.
  */
 const gulp=require('gulp');
-const sass=require('gulp-sass');
+const scss=require('gulp-sass');
 const autoprefixer=require('gulp-autoprefixer');
 const minifycss=require('gulp-minify-css');
 const babel=require('gulp-babel');
@@ -15,8 +15,8 @@ class phone {
         this.basePath=`${__dirname}/public/phone`;
         this.devPath=`${this.basePath}/dev`;
         this.distPath=`${this.basePath}/dist`;
-        this.sassEnterPath=`${this.devPath}/sass/**/*.scss`;
-        this.sassExitPath=`${this.distPath}/sass/`;
+        this.scssEnterPath=`${this.devPath}/scss/**/*.scss`;
+        this.scssExitPath=`${this.distPath}/scss/`;
         this.jsEnterPath=`${this.devPath}/js/**/*.js`;
         this.jsExitPath=`${this.distPath}/js/`;
         this.imagesEnterPath=`${this.devPath}/images/**/*.*`;
@@ -28,8 +28,8 @@ class pc {
         this.basePath=`${__dirname}/public/pc`;
         this.devPath=`${this.basePath}/dev`;
         this.distPath=`${this.basePath}/dist`;
-        this.sassEnterPath=`${this.devPath}/sass/**/*.scss`;
-        this.sassExitPath=`${this.distPath}/sass/`;
+        this.scssEnterPath=`${this.devPath}/scss/**/*.scss`;
+        this.scssExitPath=`${this.distPath}/scss/`;
         this.jsEnterPath=`${this.devPath}/js/**/*.js`;
         this.jsExitPath=`${this.distPath}/js/`;
         this.imagesEnterPath=`${this.devPath}/images/**/*.*`;
@@ -42,75 +42,73 @@ function task(opt){
     }
     const path=opt.path;
     const type=opt.type;
-    //sass
-    gulp.task(`${type}sass`,function(){
-        return gulp.src(path.sassEnterPath)
-            .pipe(sass())
+    //合并
+    gulp.task(`${type}concatScss`,function(){//scss合并
+        return gulp.src([`${path.distPath}/scss/base/base.css`,`${path.distPath}/scss/page/index.css`])
+            .pipe(concat('common.css'))
+            .pipe(gulp.dest(`${path.distPath}/scss/common/`));
+    });
+    gulp.task(`${type}concatJs`,function(){//js合并
+        return gulp.src([`${path.distPath}/js/base/base.js`,`${path.distPath}/js/page/index.js`])
+            .pipe(concat('common.js'))
+            .pipe(gulp.dest(`${path.distPath}/js/common/`));
+    });
+    //开发
+    gulp.task(`${type}scss`,function(){//scss转移
+        return gulp.src(path.scssEnterPath)
+            .pipe(scss())
             .pipe(base64())
             .pipe(autoprefixer('last 2 version','safari 5','ie 9','opera 12.1','ios 6','android 4'))
-            .pipe(gulp.dest(path.sassExitPath))
+            .pipe(gulp.dest(path.scssExitPath))
     });
-    gulp.task(`${type}sass:watch`,function(){
-        gulp.watch(path.sassEnterPath,[`${type}sass`]);
-    });
-    gulp.task(`${type}sassmin`,function(){
-        return gulp.src(path.sassEnterPath)
-            .pipe(sass())
-            .pipe(base64())
-            .pipe(autoprefixer('last 2 version','safari 5','ie 9','opera 12.1','ios 6','android 4'))
-            .pipe(minifycss())
-            .pipe(gulp.dest(path.sassExitPath))
-    });
-    gulp.task(`${type}sassmin:watch`,function(){
-        gulp.watch(path.sassEnterPath,[`${type}sassmin`]);
-    });
-    //js
-    gulp.task(`${type}js`,function(){
+    gulp.task(`${type}js`,function(){//js转移
         return gulp.src(path.jsEnterPath)
             .pipe(babel({presets:['es2015']}))
             .pipe(gulp.dest(path.jsExitPath))
     });
-    gulp.task(`${type}js:watch`,function(){
-        gulp.watch(path.jsEnterPath,[`${type}js`]);
+    gulp.task(`${type}images`,function(){//images转移
+        return gulp.src(path.imagesEnterPath)
+            .pipe(gulp.dest(path.imagesExitPath))
     });
-    gulp.task(`${type}jsmin`,function(){
+    //开发监听
+    gulp.task(`${type}:watch`,function(){
+        gulp.watch(path.scssEnterPath,[`${type}scss`]);//scss监听
+        gulp.watch(path.jsEnterPath,[`${type}js`]);//js监听
+        gulp.watch(path.imagesEnterPath,[`${type}images`]);//images监听
+    });
+    //压缩
+    gulp.task(`${type}scssMin`,function(){//scss压缩
+        return gulp.src(path.scssEnterPath)
+            .pipe(scss())
+            .pipe(base64())
+            .pipe(autoprefixer('last 2 version','safari 5','ie 9','opera 12.1','ios 6','android 4'))
+            .pipe(minifycss())
+            .pipe(gulp.dest(path.scssExitPath))
+    });
+    gulp.task(`${type}jsMin`,function(){//js压缩
         return gulp.src(path.jsEnterPath)
             .pipe(babel({presets:['es2015']}))
             .pipe(uglify())
             .pipe(gulp.dest(path.jsExitPath))
     });
-    gulp.task(`${type}jsmin:watch`,function(){
-        gulp.watch(path.jsEnterPath,[`${type}jsmin`]);
-    });
-    //images
-    gulp.task(`${type}images`,function(){
-        return gulp.src(path.imagesEnterPath)
-            .pipe(gulp.dest(path.imagesExitPath))
-    });
-    gulp.task(`${type}images:watch`,function(){
-        gulp.watch(path.imagesEnterPath,[`${type}images`]);
-    });
-    gulp.task(`${type}imagesmin`,function(){
+    gulp.task(`${type}imagesMin`,function(){//images压缩
         return gulp.src(path.imagesEnterPath)
             .pipe(imagemin())
             .pipe(gulp.dest(path.imagesExitPath))
     });
-    gulp.task(`${type}imagesmin:watch`,function(){
-        gulp.watch(path.imagesEnterPath,[`${type}imagesmin`]);
+    //压缩监听
+    gulp.task(`${type}min:watch`,function(){
+        gulp.watch(path.scssEnterPath,[`${type}scssMin`]);//scss监听
+        gulp.watch(path.jsEnterPath,[`${type}jsMin`]);//js监听
+        gulp.watch(path.imagesEnterPath,[`${type}imagesMin`]);//images监听
     });
-
-    //concat(有问题待续...前台和后台怎么分...应该是两个项目)
-    gulp.task(`${type}concat`,function(){
-        return gulp.src([`${path.distPath}/js/base/base.js`,`${path.distPath}/js/page/index.js`])
-            .pipe(concat('common.js'))
-            .pipe(gulp.dest(`${path.distPath}/js/`));
+    //执行任务
+    gulp.task(`${type}Dev`,[`${type}scss`,`${type}js`,`${type}images`,`${type}:watch`],function(){//开发
+        gulp.start(`${type}concatScss`,`${type}concatJs`);
     });
-    gulp.task(`${type}concat:watch`,function(){
-        //gulp.watch(path.sassEnterPath,[`${type}concat`]);
+    gulp.task(`${type}Build`,[`${type}scssMin`,`${type}jsMin`,`${type}imagesMin`,`${type}min:watch`],function(){//上线
+        gulp.start(`${type}concatScss`,`${type}concatJs`);
     });
-    //task
-    gulp.task(`${type}Dev`,[`${type}sass`,`${type}sass:watch`,`${type}js`,`${type}js:watch`,`${type}images`,`${type}images:watch`,`${type}concat`,`${type}concat:watch`]);
-    gulp.task(`${type}Build`,[`${type}sassmin`,`${type}sassmin:watch`,`${type}jsmin`,`${type}jsmin:watch`,`${type}imagesmin`,`${type}imagesmin:watch`,`${type}concat`,`${type}concat:watch`]);
 }
 task({path:new phone(),type:'phone'});
 task({path:new pc(),type:'pc'});
