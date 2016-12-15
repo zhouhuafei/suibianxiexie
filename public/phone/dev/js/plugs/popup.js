6a559147fa62efd83e683d10c2417b5371cec8e4
zhouhuafei <1123486116@qq.com>
2016-12-15 22:37:08 +0800

    字体图标引入失败

--- public/phone/dev/js/plugs/popup.js
+++ public/phone/dev/js/plugs/popup.js
@@ -1,21 +1,6 @@
 /**
  * Created by zhouhuafei on 16/12/13.
  */
-// export default class {
-//     constructor(){
-//
-//     }
-//     init(){
-//
-//     }
-//     alert(){
-//         console.log('alert');
-//     }
-//     confirm(){
-//         console.log('confirm');
-//     }
-// }
 module.exports=function(){
-    console.log(123457890);
-    
+    console.log('模块化测试');
 };
---
+++ public/phone/dist/js/page/index.js
@@ -0,0 +1,28 @@
+"use strict";
+
+(function e(t, n, r) {
+  function s(o, u) {
+    if (!n[o]) {
+      if (!t[o]) {
+        var a = typeof require == "function" && require;if (!u && a) return a(o, !0);if (i) return i(o, !0);throw new Error("Cannot find module '" + o + "'");
+      }var f = n[o] = { exports: {} };t[o][0].call(f.exports, function (e) {
+        var n = t[o][1][e];return s(n ? n : e);
+      }, f, f.exports, e, t, n, r);
+    }return n[o].exports;
+  }var i = typeof require == "function" && require;for (var o = 0; o < r.length; o++) {
+    s(r[o]);
+  }return s;
+})({ 1: [function (require, module, exports) {
+    /**
+     * Created by zhouhuafei on 16/12/4.
+     */
+    var Popup = require('../plugs/popup');
+    Popup();
+  }, { "../plugs/popup": 2 }], 2: [function (require, module, exports) {
+    /**
+     * Created by zhouhuafei on 16/12/13.
+     */
+    module.exports = function () {
+      console.log('模块化测试');
+    };
+  }, {}] }, {}, [1]);
