b9779dd50b3def6d3dfa8e55fbb27a6e9f9e9e66
zhouhuafei <1123486116@qq.com>
2016-12-20 18:14:23 +0800

    Update m-product.js

--- public/phone/dev/js/page/m-product.js
+++ public/phone/dev/js/page/m-product.js
@@ -2,5 +2,36 @@
  * Created by zhouhuafei on 16/12/17.
  */
 const Product=require('../modules/m-product');
-const product=new Product({configData:{isShowGoodsName:true}});
-console.log(product);
+const product=new Product({
+    configData:{
+        isShowGoodsName:true,
+        isShowPrice:true,
+        isVipCustom:true,
+        isShowLikeNum:true,
+        //isShowCart:true,
+        isShowSeckillMark:true,
+        isShowSeckillLogo:true,
+        //isShowSeckillWillBeginBtn:true,
+        isShowSeckillWillBeginTime:true,
+        isShowSeckillHintBtn:true,
+        //isShowSeckillNowGetBtn:true,
+        //isShowSeckillWillEndTime:true
+    },
+    ajaxData:{
+        goodsName:'商品名称商品名称商品名称商品名称商品名称商品名称',
+        marketPrice:'200.00',
+        nowPrice:'100.00',
+        vipPrice:'10.00',
+        seckillPrice:'1.00',
+        likeNum:'300',
+        imgSrc:'http://qmfx-s84664.s3.fy.shopex.cn/gpic/20160627/be7c4eafe8063a94bf2da631299bec6b.jpg?imageView2/2/w/600/h/600/interlace/1',
+        aHref:'www.baidu.com',
+        seckillWillBeginTime:'6',
+        seckillWillBeginBtnShowTime:'3',
+        seckillWillEndTime:'3'
+    }
+});
+product.render(function(dom){
+    product.init();
+    document.querySelector('.main').appendChild(dom);
+});
