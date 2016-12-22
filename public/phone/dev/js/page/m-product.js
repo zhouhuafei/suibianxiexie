29ddc3b66ed6475df79797281a98b3c3ebbe00d4
zhouhuafei <1123486116@qq.com>
2016-12-22 13:49:09 +0800

    Update m-product.js

--- public/phone/dev/js/page/m-product.js
+++ public/phone/dev/js/page/m-product.js
@@ -34,7 +34,7 @@ for(var i=0;i<200;i++){
             seckillWillEndTime:'3'
         }
     });
-    product.render(function(dom){
-        main.appendChild(dom);
+    product.render(function(opt){
+        main.appendChild(opt.dom);
     });
 }
