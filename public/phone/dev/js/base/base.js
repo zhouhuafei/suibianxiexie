851f1425ba289a1af8cef722aa1984bb44200954
zhouhuafei <1123486116@qq.com>
2016-12-28 18:57:31 +0800

    Update base.js

--- public/phone/dev/js/base/base.js
+++ public/phone/dev/js/base/base.js
@@ -1,6 +1,54 @@
 /**
  * Created by zhouhuafei on 16/12/4.
  */
+
+//是不是空
+            var isSpace=function(opt){
+                var obj=opt||{};
+                var value=obj.value||" ";
+                var valueTrim=value.trim();
+                var b=false;
+                if(valueTrim==''){
+                    b=true;
+                }
+                return b;
+            };
+            //是不是0
+            var isZero=function(opt){
+                var obj=opt||{};
+                var value=obj.value||" ";
+                var valueTrim=value.trim();
+                var b=false;
+                if(valueTrim==0){
+                    b=true;
+                }
+                return b;
+            };
+            //是不是正整数
+            var isPositiveInteger=function(opt){
+                var obj=opt||{};
+                var value=obj.value||" ";
+                var valueTrim=value.trim();
+                var re=/^\d+$/;
+                var b=false;
+                if(re.test(valueTrim)){
+                    b=true;
+                }
+                return b;
+            };
+            //是不是保留了num位小数
+            var isReservedDecimal=function(opt){
+                var obj=opt||{};
+                var num=obj.num||2;
+                var value=obj.value||" ";
+                var valueTrim=value.trim();
+                var re=new RegExp("^\\d+\\.\\d{"+num+"}$");
+                var b=false;
+                if(re.test(valueTrim)){
+                    b=true;
+                }
+                return b;
+            };
 var base={};
 base.goTop=function(opt){//返回顶部
     var obj=opt.obj;
