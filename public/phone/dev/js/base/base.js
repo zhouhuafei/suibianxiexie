de4ef7ca0248a37ab86c1603642b7aacdb710e1c
zhouhuafei <1123486116@qq.com>
2016-12-30 15:26:42 +0800

    Update base.js

--- public/phone/dev/js/base/base.js
+++ public/phone/dev/js/base/base.js
@@ -1,6 +1,31 @@
 /**
  * Created by zhouhuafei on 16/12/4.
  */
+        //设置cookie
+        function setCookie(name,value,expires){
+            var myDate=new Date();
+            var myTime=myDate.getTime();
+            myDate.setTime(myTime+expires*24*60*60*1000);
+            document.cookie=name+'='+value+'; expires='+myDate;
+        }
+        //获取cookie
+        function getCookie(name){
+            var cookie=document.cookie;
+            var arr=cookie.split('; ');
+            var value='';
+            arr.forEach(function(v){
+                var arr2=v.split('=');
+                if(arr2[0]==name){
+                    value=arr2[1];
+                    return false;
+                }
+            });
+            return value;
+        }
+        //清除cookie
+        function removeCookie(name){
+            setCookie(name,'',-1);
+        }
 //字符数量限制
             var limitTxt=function(opt){
                 if(!opt){
