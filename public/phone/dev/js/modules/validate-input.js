c5fee6df88e1db760b5e701c526c63ec90666d92
zhouhuafei <1123486116@qq.com>
2017-01-03 00:19:02 +0800

    js

--- public/phone/dev/js/modules/validate-input.js
+++ public/phone/dev/js/modules/validate-input.js
@@ -4,9 +4,11 @@
 function ValidateInput(json){
     this.opt=json||{};
     this.input=this.opt.input;
+    this.parentClass=this.opt.parentClass||'m-validate-input-parent';
+    this.hintClass=this.opt.hintClass||'m-validate-input-hint';
+    this.errorClass=this.opt.errorClass||'m-validate-input-error';
     this.validateType=this.input.dataset.validate||[];
     this.validateHintTxt=this.input.dataset.hint||[];
-    this.inputClassError=this.opt.inputClassError||'m-validate-input-error';
     this.init();
 }
 ValidateInput.prototype.init=function(){
@@ -22,52 +24,70 @@ ValidateInput.prototype.render=function(){
 };
 ValidateInput.prototype.renderParent=function(){
     this.parentDom=this.input.parentNode;
-    this.parentDom.classList.add('m-validate-input-parent');
+    this.parentDom.classList.add(this.parentClass);
 };
 ValidateInput.prototype.renderHint=function(){
     this.hintDom=document.createElement('em');
-    this.hintDom.classList.add('m-validate-input-hint');
+    this.hintDom.classList.add(this.hintClass);
 };
 ValidateInput.prototype.renderHintAdd=function(json){
     var opt=json||{};
     this.hintDom.innerHTML=opt.txt||'本项必填';
     this.parentDom.appendChild(this.hintDom);
-    this.input.classList.add(this.inputClassError);
+    this.input.classList.add(this.errorClass);
 };
 ValidateInput.prototype.renderHintRemove=function(){
-    this.parentDom.removeChild(this.hintDom);
-    this.input.classList.remove(this.inputClassError);
+    var isHaveHintDom=this.parentDom.querySelector(`.${this.hintClass}`);
+    if(isHaveHintDom){
+        this.parentDom.removeChild(this.hintDom);
+    }
+    this.input.classList.remove(this.errorClass);
 };
 ValidateInput.prototype.validateSave=function(){
     var self=this;
     var type=self.validateType.split(' ');
     var hintTxt=self.validateHintTxt.split(' ');
     var value=this.input.value;
+    var isTrue=true;
     type.forEach(function(v,i){
-        if(v=='no-space'){//设置了非空验证
+        if(v=='no-space'&&isTrue){//设置了非空验证
             self.validate.isSpace({
                 value:value,
                 success:function(){//空
                     self.renderHintAdd({txt:hintTxt[i]});
+                    isTrue=false;
                 },
                 fail:function(){//非空
                     self.renderHintRemove();
+                    isTrue=true;
                 }
             });
         }
-        if(v=='no-zero'){//设置了非零验证
-            self.validate.isZero({success:function(){//零
-                self.renderHintAdd({txt:hintTxt[i]});
-            },fail:function(){//非零
-                self.renderHintRemove();
-            }});
+        if(v=='no-zero'&&isTrue){//设置了非零验证
+            self.validate.isZero({
+                value:value,
+                success:function(){//零
+                    self.renderHintAdd({txt:hintTxt[i]});
+                    isTrue=false;
+                },
+                fail:function(){//非零
+                    self.renderHintRemove();
+                    isTrue=true;
+                }
+            });
         }
-        if(v=='yes-integer'){//设置了整数验证
-            self.validate.isInteger({success:function(){//整数
-                self.renderHintRemove();
-            },fail:function(){//非整数
-                self.renderHintAdd({txt:hintTxt[i]});
-            }});
+        if(v=='yes-integer'&&isTrue){//设置了整数验证
+            self.validate.isInteger({
+                value:value,
+                success:function(){//整数
+                    self.renderHintRemove();
+                    isTrue=true;
+                },
+                fail:function(){//非整数
+                    self.renderHintAdd({txt:hintTxt[i]});
+                    isTrue=false;
+                }
+            });
         }
     });
 };
--- public/phone/dist/js/page/index.js
+++ public/phone/dist/js/page/index.js
@@ -1204,9 +1204,11 @@
         function ValidateInput(json) {
             this.opt = json || {};
             this.input = this.opt.input;
+            this.parentClass = this.opt.parentClass || 'm-validate-input-parent';
+            this.hintClass = this.opt.hintClass || 'm-validate-input-hint';
+            this.errorClass = this.opt.errorClass || 'm-validate-input-error';
             this.validateType = this.input.dataset.validate || [];
             this.validateHintTxt = this.input.dataset.hint || [];
-            this.inputClassError = this.opt.inputClassError || 'm-validate-input-error';
             this.init();
         }
         ValidateInput.prototype.init = function () {
@@ -1222,61 +1224,79 @@
         };
         ValidateInput.prototype.renderParent = function () {
             this.parentDom = this.input.parentNode;
-            this.parentDom.classList.add('m-validate-input-parent');
+            this.parentDom.classList.add(this.parentClass);
         };
         ValidateInput.prototype.renderHint = function () {
             this.hintDom = document.createElement('em');
-            this.hintDom.classList.add('m-validate-input-hint');
+            this.hintDom.classList.add(this.hintClass);
         };
         ValidateInput.prototype.renderHintAdd = function (json) {
             var opt = json || {};
             this.hintDom.innerHTML = opt.txt || '本项必填';
             this.parentDom.appendChild(this.hintDom);
-            this.input.classList.add(this.inputClassError);
+            this.input.classList.add(this.errorClass);
         };
         ValidateInput.prototype.renderHintRemove = function () {
-            this.parentDom.removeChild(this.hintDom);
-            this.input.classList.remove(this.inputClassError);
+            var isHaveHintDom = this.parentDom.querySelector("." + this.hintClass);
+            if (isHaveHintDom) {
+                this.parentDom.removeChild(this.hintDom);
+            }
+            this.input.classList.remove(this.errorClass);
         };
         ValidateInput.prototype.validateSave = function () {
             var self = this;
             var type = self.validateType.split(' ');
             var hintTxt = self.validateHintTxt.split(' ');
             var value = this.input.value;
+            var isTrue = true;
             type.forEach(function (v, i) {
-                if (v == 'no-space') {
+                if (v == 'no-space' && isTrue) {
                     //设置了非空验证
                     self.validate.isSpace({
                         value: value,
                         success: function success() {
                             //空
                             self.renderHintAdd({ txt: hintTxt[i] });
+                            isTrue = false;
                         },
                         fail: function fail() {
                             //非空
                             self.renderHintRemove();
+                            isTrue = true;
                         }
                     });
                 }
-                if (v == 'no-zero') {
+                if (v == 'no-zero' && isTrue) {
                     //设置了非零验证
-                    self.validate.isZero({ success: function success() {
+                    self.validate.isZero({
+                        value: value,
+                        success: function success() {
                             //零
                             self.renderHintAdd({ txt: hintTxt[i] });
-                        }, fail: function fail() {
+                            isTrue = false;
+                        },
+                        fail: function fail() {
                             //非零
                             self.renderHintRemove();
-                        } });
+                            isTrue = true;
+                        }
+                    });
                 }
-                if (v == 'yes-integer') {
+                if (v == 'yes-integer' && isTrue) {
                     //设置了整数验证
-                    self.validate.isInteger({ success: function success() {
+                    self.validate.isInteger({
+                        value: value,
+                        success: function success() {
                             //整数
                             self.renderHintRemove();
-                        }, fail: function fail() {
+                            isTrue = true;
+                        },
+                        fail: function fail() {
                             //非整数
                             self.renderHintAdd({ txt: hintTxt[i] });
-                        } });
+                            isTrue = false;
+                        }
+                    });
                 }
             });
         };
