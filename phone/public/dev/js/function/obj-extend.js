31dbf77000471a4297031cbed30532b5b277e44f
zhouhuafei <1123486116@qq.com>
2017-02-23 15:03:58 +0800

    Update obj-extend.js

--- phone/public/dev/js/function/obj-extend.js
+++ phone/public/dev/js/function/obj-extend.js
@@ -10,3 +10,5 @@ function extend(json) {
     return defaults;
 }
 module.exports = extend;
+//有BUG,例如extend({default:{a:0,b:{b1:1}},inherit:{a:0,b:{b2:2}}});
+//得到的结果{a:0,b:{b2:2}},b1的数据就没了,正确的结果应该保留{a:0,b:{b1:1,b2:2}}
