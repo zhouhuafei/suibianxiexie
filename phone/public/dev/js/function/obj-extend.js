0118970491e321c471c9cc0f6be4036de300ea1f
zhouhuafei <1123486116@qq.com>
2017-02-24 18:59:34 +0800

    Update obj-extend.js

--- phone/public/dev/js/function/obj-extend.js
+++ phone/public/dev/js/function/obj-extend.js
@@ -1,14 +1,14 @@
 function extend(json) {
     var opt = json || {};
     var defaults = opt.default||{};
-    var inherit = opt.inherit||{};
-    for (var attr in inherit) {
-        if (inherit.hasOwnProperty(attr)) {
-            defaults[attr] = JSON.parse(JSON.stringify(inherit[attr]));
+    var inherits = opt.inherits||{};
+    for (var attr in inherits) {
+        if (inherits.hasOwnProperty(attr)) {
+            defaults[attr] = JSON.parse(JSON.stringify(inherits[attr]));
         }
     }
     return defaults;
 }
 module.exports = extend;
-//有BUG,例如extend({default:{a:0,b:{b1:1}},inherit:{a:0,b:{b2:2}}});
+//有BUG,例如extend({default:{a:0,b:{b1:1}},inherits:{a:0,b:{b2:2}}});
 //得到的结果{a:0,b:{b2:2}},b1的数据就没了,正确的结果应该保留{a:0,b:{b1:1,b2:2}}
