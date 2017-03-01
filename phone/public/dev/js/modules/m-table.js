472ddd423f482977098edff46fd34950dd22867a
zhouhuafei <1123486116@qq.com>
2017-03-01 12:59:17 +0800

    Update m-table.js

--- phone/public/dev/js/modules/m-table.js
+++ phone/public/dev/js/modules/m-table.js
@@ -11,32 +11,48 @@ Fn.prototype.render = function () {
     this.parentDom.innerHTML=`
         <div class="m-table-header">
             <div class="m-table-row">
-                <div class="m-table-col">
-                    <div class="m-table-col-wrap">0</div>
-                </div>
-                <div class="m-table-col">
-                    <div class="m-table-col-wrap">1</div>
-                </div>
-                <div class="m-table-col">
-                    <div class="m-table-col-wrap">2</div>
-                </div>
+                ${this.renderHeader()}
             </div>
         </div>
         <div class="m-table-body">
-            <div class="m-table-row">
-                <div class="m-table-col">
-                    <div class="m-table-col-wrap">0</div>
-                </div>
-                <div class="m-table-col">
-                    <div class="m-table-col-wrap">1</div>
-                </div>
-                <div class="m-table-col">
-                    <div class="m-table-col-wrap">2</div>
-                </div>
-            </div>
+            ${this.renderBody()}
         </div>
-        <div class="m-table-footer"></div>
+        <div class="m-table-footer">
+            ${this.renderFooter()}
+        </div>
     `;
 };
-//待续...
+Fn.prototype.renderHeader=function(){
+    var html=``;
+    this.opt.header.forEach(function(v){
+        html+=`
+            <div class="m-table-col">
+                <div class="m-table-col-wrap">
+                    ${v.html}
+                </div>
+            </div>
+        `;
+    });
+    return html;
+};
+Fn.prototype.renderBody=function(){
+    var html=``;
+    this.opt.body.forEach(function(v0){
+        var row=``;
+        v0.forEach(function(v1){
+            row+=`
+                <div class="m-table-col">
+                    <div class="m-table-col-wrap">
+                        ${v1.html}
+                    </div>
+                </div>
+            `;
+        });
+        html+=`<div class="m-table-row">${row}</div>`;
+    });
+    return html;
+};
+Fn.prototype.renderFooter=function(){
+    return this.opt.footer;
+};
 module.exports = Fn;
