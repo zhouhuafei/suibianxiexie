e550eb441495a3163cb570a507896177f44ccc2d
zhouhuafei <1123486116@qq.com>
2017-03-01 14:13:42 +0800

    Update other.js

--- phone/public/dev/js/base/other.js
+++ phone/public/dev/js/base/other.js
@@ -1,3 +1,122 @@
+/*
+
+.ui-radio-switch{
+  cursor: pointer;
+  display: -webkit-box;
+  height: 20px;
+  line-height: 20px;
+  .ui-radio-switch-box{
+    position: relative;
+    width: 40px;
+    height: 100%;
+    text-align: center;
+    line-height: 20px;
+    border-radius: 100px;
+    background: #bbbbbb;
+  }
+  .ui-radio-switch-run{
+    width: 18px;
+    height: 18px;
+    background: #ffffff;
+    border-radius: 100px;
+    position: absolute;
+    left: 1px;
+    top: 1px;
+    transition: 0.4s;
+  }
+  .ui-radio-switch-txt{
+    margin-left: 4px;
+  }
+  &.ui-radio-switch-active{
+    .ui-radio-switch-box{
+      background: $color-success;
+    }
+    .ui-radio-switch-run{
+      position: absolute;
+      left: calc(100% - 19px);
+    }
+    .ui-radio-switch-txt{
+      color: #1fce69;
+    }
+  }
+}
+
+
+function Fn(json){
+        this.opts=$.extend(true,{
+            checkTxt:{
+                on:'已开启',
+                off:'已关闭',
+            },
+            status:'off',//'off'关  'on'开
+            isHand:true,//默认手动触发划过去
+            clickCallback:function(){
+                console.log('点击的回调');
+            }
+        },json);
+        this.onClass='ui-radio-switch-active';//打开时对应状态的class
+        this.init();
+    }
+    Fn.prototype.init=function(){
+        this.render();
+        this.events();
+    };
+    Fn.prototype.render=function(){
+        var className=``;
+        var status=this.opts.status;
+        if(status=='on'){
+            className=this.onClass;
+        }
+        this.parent=$(`<div class="ui-radio-switch ${className}"></div>`);
+        var html=`            
+            <div class="ui-radio-switch-box">
+                <div class="ui-radio-switch-run"></div>
+            </div>
+            <div class="ui-radio-switch-txt">${this.opts.checkTxt[status]}</div>            
+        `;
+        this.parent.html(html);
+    };
+    Fn.prototype.on=function(){//开
+        this.parent.addClass(this.onClass);
+        this.opts.status='on';
+        this.changeTxt();
+    };
+    Fn.prototype.off=function(){//关
+        this.parent.removeClass(this.onClass);
+        this.opts.status='off';
+        this.changeTxt();
+    };
+    Fn.prototype.changeTxt=function(){
+        this.parent.find('.ui-radio-switch-txt').html(this.opts.checkTxt[this.opts.status]);
+    };
+    Fn.prototype.clickFn=function(){
+        var self=this;
+        if(!self.opts.isHand){
+            if(self.opts.status=='off'){
+                self.on();
+            }else{
+                self.off();
+            }
+        }
+        self.opts.clickCallback({
+            parent:this.parent,
+            status:self.opts.status,
+        });
+    };
+    Fn.prototype.events=function(){
+        var self=this;
+        this.parent.on('click',function(){
+            self.clickFn();
+        })
+    };
+    Fn.prototype.remove=function(){
+        this.parent.remove();
+    };
+    module.exports = Fn;
+*/
+
+
+
 function uploadImg() {
     var oF = document.querySelector('#fileField');
     var num = 0;
