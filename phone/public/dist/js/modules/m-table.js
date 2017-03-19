2367882eb70fc7869e80bf4b197c5a7017e441e8
zhouhuafei <1123486116@qq.com>
2017-03-20 00:13:18 +0800

    create-element

--- phone/public/dist/js/modules/m-table.js
+++ phone/public/dist/js/modules/m-table.js
@@ -219,21 +219,21 @@
          */
         function createElement(json) {
             var opt = json || {};
-            opt.tagName = opt.tagName || 'div'; //普通属性
-            opt.attribute = opt.attribute || {}; //自定义属性,checked,selected
-            opt.custom = opt.custom || {};
-            var tag = document.createElement("" + opt.tagName);
+            opt.elementName = opt.elementName || 'div'; //标签名称
+            opt.attribute = opt.attribute || {}; //普通属性,checked,selected
+            opt.custom = opt.custom || {}; //自定义属性
+            var elementNode = document.createElement("" + opt.elementName); //元素节点
             for (var attr0 in opt.attribute) {
                 if (opt.attribute.hasOwnProperty(attr0)) {
-                    tag[attr0] = opt.attribute[attr0];
+                    elementNode[attr0] = opt.attribute[attr0];
                 }
             }
             for (var attr1 in opt.custom) {
                 if (opt.custom.hasOwnProperty(attr1)) {
-                    tag.setAttribute('data-' + attr1, opt.custom[attr1]);
+                    elementNode.setAttribute('data-' + attr1, opt.custom[attr1]);
                 }
             }
-            return tag;
+            return elementNode;
         }
         module.exports = createElement;
     }, {}], 5: [function (require, module, exports) {
