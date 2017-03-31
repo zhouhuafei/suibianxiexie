1b17c101b78040a0cb476cfafd6795f5d4926327
zhouhuafei <1123486116@qq.com>
2017-03-31 16:56:43 +0800

    Update m-footer.js

--- phone/public/dev/js/modules/m-footer.js
+++ phone/public/dev/js/modules/m-footer.js
@@ -39,26 +39,28 @@ SubType.prototype.moduleDomType0=function(){
     if(this.opt.config.moduleDomType=='0'){
         this.moduleDomClass=`m-footer m-footer-type0`;
         return `
-            <div class="m-footer-header">
-                <div class="m-footer-header-icon icons icons-store"></div>
-            </div>
-            <div class="m-footer-body m-footer-body-active">
-                <div class="m-footer-body-icon icons icons-jifen"></div>
-                <div class="m-footer-body-txt">全部商品</div>
-                <div class="m-footer-body-child">
-                    <div class="m-footer-body-child-item"><a href="">child-item</a></div>
-                    <div class="m-footer-body-child-item"><a href="">child-item</a></div>
+            <div class="m-footer-wrap">
+                <div class="m-footer-header">
+                    <div class="m-footer-header-icon icons icons-store"></div>
                 </div>
-            </div>
-            <a class="m-footer-body" href="">
-                <div class="m-footer-body-txt">上新</div>      
-            </a>
-            <div class="m-footer-body">
-                <div class="m-footer-body-icon icons icons-jifen"></div>
-                <div class="m-footer-body-txt">店铺活动</div>
-                <div class="m-footer-body-child">
-                    <div class="m-footer-body-child-item"><a href="">child-item</a></div>
-                    <div class="m-footer-body-child-item"><a href="">child-item</a></div>
+                <div class="m-footer-body m-footer-body-active">
+                    <div class="m-footer-body-icon icons icons-jifen"></div>
+                    <div class="m-footer-body-txt">全部商品</div>
+                    <div class="m-footer-body-child">
+                        <div class="m-footer-body-child-item"><a href="">child-item</a></div>
+                        <div class="m-footer-body-child-item"><a href="">child-item</a></div>
+                    </div>
+                </div>
+                <a href="" class="m-footer-body">
+                    <div class="m-footer-body-txt">上新</div>      
+                </a>
+                <div class="m-footer-body">
+                    <div class="m-footer-body-icon icons icons-jifen"></div>
+                    <div class="m-footer-body-txt">店铺活动</div>
+                    <div class="m-footer-body-child">
+                        <div class="m-footer-body-child-item"><a href="">child-item</a></div>
+                        <div class="m-footer-body-child-item"><a href="">child-item</a></div>
+                    </div>
                 </div>
             </div>
         `;
@@ -70,26 +72,28 @@ SubType.prototype.moduleDomType1=function(){
     if(this.opt.config.moduleDomType=='1'){
         this.moduleDomClass=`m-footer m-footer-type1`;        
         return `
-            <a class="m-footer-body m-footer-body-active" href="">
-                <div class="m-footer-body-icon icons icons-santiaogang"></div>
-                <div class="m-footer-body-txt">首页</div>
-            </a>
-            <a class="m-footer-body" href="">
-                <div class="m-footer-body-icon icons icons-shoucang"></div>
-                <div class="m-footer-body-txt">我要开店</div>
-            </a>
-            <a class="m-footer-body" href="">
-                <div class="m-footer-body-icon icons icons-shouji"></div>
-                <div class="m-footer-body-txt">购物车</div>
-            </a>
-            <a class="m-footer-body" href="">
-                <div class="m-footer-body-icon icons icons-cart"></div>
-                <div class="m-footer-body-txt">客服</div>
-            </a>
-            <a class="m-footer-body" href="">
-                <div class="m-footer-body-icon icons icons-jifen"></div>
-                <div class="m-footer-body-txt">我的</div>
-            </a>
+            <div class="m-footer-wrap">
+                <a class="m-footer-body m-footer-body-active">
+                    <div class="m-footer-body-icon icons icons-santiaogang"></div>
+                    <div class="m-footer-body-txt">首页</div>
+                </a>
+                <a class="m-footer-body">
+                    <div class="m-footer-body-icon icons icons-shoucang"></div>
+                    <div class="m-footer-body-txt">我要开店</div>
+                </a>
+                <a class="m-footer-body">
+                    <div class="m-footer-body-icon icons icons-shouji"></div>
+                    <div class="m-footer-body-txt">购物车</div>
+                </a>
+                <a class="m-footer-body">
+                    <div class="m-footer-body-icon icons icons-cart"></div>
+                    <div class="m-footer-body-txt">客服</div>
+                </a>
+                <a class="m-footer-body">
+                    <div class="m-footer-body-icon icons icons-jifen"></div>
+                    <div class="m-footer-body-txt">我的</div>
+                </a>
+            </div>
         `;
     }
     return ``;    
