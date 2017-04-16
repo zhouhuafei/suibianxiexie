a9c71df01e5d62f8e4f3d16d6f1dc32f90f34be4
zhouhuafei <1123486116@qq.com>
2017-04-16 20:19:04 +0800

    html

--- phone/gulpfile.js
+++ phone/gulpfile.js
@@ -37,48 +37,13 @@ const path = new Path();
 gulp.task(`del`, function () {
     return del.sync([`${path.distPath}`]);
 });
-//转移
-gulp.task(`font`, function () {//font转移
-    return gulp.src(path.fontEnterPath)
-        .pipe(gulp.dest(path.fontExitPath))
-});
-gulp.task(`ui`, function () {//ui转移
-    return gulp.src(path.uiEnterPath)
-        .pipe(gulp.dest(path.uiExitPath))
-});
+//html
 gulp.task(`html`, function () {//html转移并压缩
     return gulp.src(path.htmlEnterPath)
         .pipe(htmlmin({collapseWhitespace: true, removeComments: true}))
         .pipe(gulp.dest(path.htmlExitPath))
 });
-//开发
-gulp.task(`scss`, function () {//scss编译
-    return gulp.src(path.scssEnterPath)
-        .pipe(scss())
-        .pipe(base64())
-        .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
-        .pipe(gulp.dest(path.scssExitPath))
-});
-gulp.task(`js`, function () {//js编译
-    return gulp.src(path.jsEnterPath)
-        .pipe(browserify())
-        .pipe(babel({presets: ['es2015']}))
-        .pipe(gulp.dest(path.jsExitPath))
-});
-gulp.task(`images`, function () {//images转移
-    return gulp.src(path.imagesEnterPath)
-        .pipe(gulp.dest(path.imagesExitPath))
-});
-//开发监听
-gulp.task(`:watch`, function () {
-    gulp.watch(path.fontEnterPath, [`font`]);//font监听
-    gulp.watch(path.uiEnterPath, [`ui`]);//ui监听
-    gulp.watch(path.htmlEnterPath, [`html`]);//html监听
-    gulp.watch(path.scssEnterPath, [`scss`]);//scss监听
-    gulp.watch(path.jsEnterPath, [`js`]);//js监听
-    gulp.watch(path.imagesEnterPath, [`images`]);//images监听
-});
-//压缩
+//css
 gulp.task(`scssMin`, function () {//scss压缩
     return gulp.src(path.scssEnterPath)
         .pipe(scss())
@@ -87,6 +52,13 @@ gulp.task(`scssMin`, function () {//scss压缩
         .pipe(minifycss())
         .pipe(gulp.dest(path.scssExitPath))
 });
+//js
+gulp.task(`js`, function () {//js编译
+    return gulp.src(path.jsEnterPath)
+        .pipe(browserify())
+        .pipe(babel({presets: ['es2015']}))
+        .pipe(gulp.dest(path.jsExitPath))
+});
 gulp.task(`jsMin`, function () {//js压缩
     return gulp.src(path.jsEnterPath)
         .pipe(browserify())
@@ -94,20 +66,43 @@ gulp.task(`jsMin`, function () {//js压缩
         .pipe(uglify())
         .pipe(gulp.dest(path.jsExitPath))
 });
+//images
+gulp.task(`images`, function () {//images转移
+    return gulp.src(path.imagesEnterPath)
+        .pipe(gulp.dest(path.imagesExitPath))
+});
 gulp.task(`imagesMin`, function () {//images压缩
     return gulp.src(path.imagesEnterPath)
         .pipe(imagemin())
         .pipe(gulp.dest(path.imagesExitPath))
 });
-//压缩监听
-gulp.task(`min:watch`, function () {
+//font
+gulp.task(`font`, function () {//font转移
+    return gulp.src(path.fontEnterPath)
+        .pipe(gulp.dest(path.fontExitPath))
+});
+//ui
+gulp.task(`ui`, function () {//ui转移
+    return gulp.src(path.uiEnterPath)
+        .pipe(gulp.dest(path.uiExitPath))
+});
+//监听
+gulp.task(`dev:watch`, function () {//开发
+    gulp.watch(path.htmlEnterPath, [`html`]);//html监听
+    gulp.watch(path.scssEnterPath, [`scssMin`]);//scss监听
+    gulp.watch(path.jsEnterPath, [`js`]);//js监听
+    gulp.watch(path.imagesEnterPath, [`images`]);//images监听
     gulp.watch(path.fontEnterPath, [`font`]);//font监听
     gulp.watch(path.uiEnterPath, [`ui`]);//ui监听
+});
+gulp.task(`min:watch`, function () {//压缩
     gulp.watch(path.htmlEnterPath, [`html`]);//html监听
     gulp.watch(path.scssEnterPath, [`scssMin`]);//scss监听
     gulp.watch(path.jsEnterPath, [`jsMin`]);//js监听
     gulp.watch(path.imagesEnterPath, [`imagesMin`]);//images监听
+    gulp.watch(path.fontEnterPath, [`font`]);//font监听
+    gulp.watch(path.uiEnterPath, [`ui`]);//ui监听
 });
 //执行任务
-gulp.task(`dev`, [`del`, `font`, `ui`, `html`, `scssMin`, `js`, `images`, `:watch`]);//开发
-gulp.task(`min`, [`del`, `font`, `ui`, `html`, `scssMin`, `jsMin`, `imagesMin`, `:watch`]);//上线
+gulp.task(`dev`, [`del`, `html`, `scssMin`, `js`, `images`, `font`, `ui`, `dev:watch`]);//开发
+gulp.task(`min`, [`del`, `html`, `scssMin`, `jsMin`, `imagesMin`, `font`, `ui`, `min:watch`]);//上线
