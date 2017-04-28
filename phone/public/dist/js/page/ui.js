59acfb69c74092c1a73d425e21bc2935422675ef
zhouhuafei <zhouhuafei@shopex.cn>
2017-04-28 12:02:45 +0800

    保持原样

--- phone/public/dist/js/page/ui.js
+++ phone/public/dist/js/page/ui.js
@@ -46,174 +46,170 @@ function _inherits(subClass, superClass) { if (typeof superClass !== "function" 
         };
         module.exports = base;
     }, { "../function/array-remove-repeat": 4, "../function/constructor-inherit": 5, "../function/cookie": 6, "../function/create-element": 7, "../function/extend": 8, "../function/fill-zero": 9, "../function/get-dom-array": 10, "../function/get-parent": 11, "../function/html-to-dom": 12, "../function/obj-remove-quote": 13, "../function/obj-to-array": 14, "../function/offset": 15, "../function/scroll-to": 16, "../function/seconds-to-time": 17, "../function/select": 18, "../function/str-limit": 19, "../function/time-count-down": 20, "../function/user-agent": 21, "../function/when-scroll-bottom": 23, "../function/whether-disable-scroll": 24 }], 2: [function (require, module, exports) {
-        window.addEventListener('load', function () {
-            setTimeout(function () {
-                //base函数测试
-                (function () {
-                    var base = require('../base/base');
-                    //测试滚动到底部loading
-                    new base.WhenScrollBottom({
-                        callback: {
-                            success: function success() {
-                                var Loading = require('../modules/m-loading');
-                                var loading = new Loading({
-                                    wrap: '.page-wrap',
-                                    config: {
-                                        status: 'loading'
-                                    }
-                                });
-                                loading.moduleDomShow();
+        //base函数测试
+        (function () {
+            var base = require('../base/base');
+            //测试滚动到底部loading
+            new base.WhenScrollBottom({
+                callback: {
+                    success: function success() {
+                        var Loading = require('../modules/m-loading');
+                        var loading = new Loading({
+                            wrap: '.page-wrap',
+                            config: {
+                                status: 'loading'
                             }
-                        }
-                    });
-                    //测试全选
-                    new base.Select({
-                        items: '.g-checkbox-checkbox',
-                        callback: {
-                            click: function click(obj) {
-                                console.log(obj);
-                            }
-                        }
-                    });
-                })();
-                //弹窗测试
-                (function () {
-                    var Dialog = require('../modules/m-dialog');
-                    //new Dialog();
-                    // new Dialog({
-                    //     config: {
-                    //         type:'confirm'
-                    //     }
-                    // });
-                })();
-                //分页测试
-                (function () {
-                    var Pagination = require('../modules/m-pagination');
-                    new Pagination({ wrap: '.page-pagination' });
-                })();
-                //没有数据
-                (function () {
-                    var NoData = require('../modules/m-no-data');
-                    new NoData({ wrap: '.page-no-data' });
-                })();
-                //加载中
-                (function () {
-                    var Loading = require('../modules/m-loading');
-                    var loading = new Loading({
-                        config: {
-                            status: 'loading'
-                        }
-                    });
-                    loading.moduleDomShow();
-                    var over = new Loading({
-                        config: {
-                            status: 'over'
-                        }
-                    });
-                    over.moduleDomShow();
-                })();
-                //超类型模块测试
-                (function () {
-                    var SuperType = require('../modules/m-super-type');
-                    new SuperType({ wrap: ".page-super-type" });
-                    var SubType = require('../modules/m-sub-type');
-                    new SubType({ wrap: ".page-super-type" });
-                    var SuperTypeEs6 = require('../modules/m-super-type-es6');
-                    new SuperTypeEs6({ wrap: ".page-super-type" });
-                    var SubTypeEs6 = require('../modules/m-sub-type-es6');
-                    new SubTypeEs6({ wrap: ".page-super-type" });
-                })();
-                //返回顶部
-                (function () {
-                    var GoTop = require('../modules/m-go-top');
-                    new GoTop();
-                })();
-                //遮罩
-                (function () {
-                    var Mask = require('../modules/m-mask');
-                    var mask = new Mask({
-                        callback: {
-                            click: function click() {
-                                mask.moduleDomHide();
-                            }
-                        }
-                    });
-                    //mask.moduleDomShow();
-                })();
-                //单选开关
-                (function () {
-                    var Radio = require('../modules/m-radio-switch');
-                    new Radio({
-                        wrap: '.page-radio-switch',
-                        callback: {
-                            click: function click(obj) {
-                                console.log(obj);
-                            }
-                        }
-                    });
-                })();
-                //表格
-                (function () {
-                    var Table = require('../modules/m-table');
-                    var table = new Table({
-                        wrap: ".page-table",
-                        data: {
-                            header: [{
-                                html: "<div>header0</div>"
-                            }, {
-                                html: "<div>header1</div>"
-                            }, {
-                                html: "<div>header2</div>"
-                            }],
-                            body: [[{
-                                html: "<div>body0-0</div>"
-                            }, {
-                                html: "<div>body1-0</div>"
-                            }, {
-                                html: "<div>body2-0</div>"
-                            }], [{
-                                html: "<div>body0-1</div>"
-                            }, {
-                                html: "<div>body1-1</div>"
-                            }, {
-                                html: "<div>body2-1</div>"
-                            }], [{
-                                html: "<div>body0-2</div>"
-                            }, {
-                                html: "<div>body1-2</div>"
-                            }, {
-                                html: "<div>body2-2</div>"
-                            }]],
-                            footer: ""
-                        }
-                    });
-                })();
-                //验证
-                (function () {
-                    var ValidateInput = require('../modules/m-validate-form');
-                    var aInput = [].slice.call(document.querySelectorAll('.m-validate-form'));
-                    aInput.forEach(function (v) {
-                        v.validate = new ValidateInput({ form: v });
-                        v.validate.validateEventBlur();
-                        //v.validate.validateSave();
-                    });
-                })();
-                //星评
-                (function () {
-                    var Star = require('../modules/m-star');
-                    var star = new Star({
-                        wrap: ".page-star",
-                        callback: {
-                            click: function click(json) {
-                                console.log(json);
-                            }
-                        }
-                    });
-                })();
-                //每个页面都要用到的js
-                require('../common/common');
-            }, 0);
-        });
+                        });
+                        loading.moduleDomShow();
+                    }
+                }
+            });
+            //测试全选
+            new base.Select({
+                items: '.g-checkbox-checkbox',
+                callback: {
+                    click: function click(obj) {
+                        console.log(obj);
+                    }
+                }
+            });
+        })();
+        //弹窗测试
+        (function () {
+            var Dialog = require('../modules/m-dialog');
+            //new Dialog();
+            // new Dialog({
+            //     config: {
+            //         type:'confirm'
+            //     }
+            // });
+        })();
+        //分页测试
+        (function () {
+            var Pagination = require('../modules/m-pagination');
+            new Pagination({ wrap: '.page-pagination' });
+        })();
+        //没有数据
+        (function () {
+            var NoData = require('../modules/m-no-data');
+            new NoData({ wrap: '.page-no-data' });
+        })();
+        //加载中
+        (function () {
+            var Loading = require('../modules/m-loading');
+            var loading = new Loading({
+                config: {
+                    status: 'loading'
+                }
+            });
+            loading.moduleDomShow();
+            var over = new Loading({
+                config: {
+                    status: 'over'
+                }
+            });
+            over.moduleDomShow();
+        })();
+        //超类型模块测试
+        (function () {
+            var SuperType = require('../modules/m-super-type');
+            new SuperType({ wrap: ".page-super-type" });
+            var SubType = require('../modules/m-sub-type');
+            new SubType({ wrap: ".page-super-type" });
+            var SuperTypeEs6 = require('../modules/m-super-type-es6');
+            new SuperTypeEs6({ wrap: ".page-super-type" });
+            var SubTypeEs6 = require('../modules/m-sub-type-es6');
+            new SubTypeEs6({ wrap: ".page-super-type" });
+        })();
+        //返回顶部
+        (function () {
+            var GoTop = require('../modules/m-go-top');
+            new GoTop();
+        })();
+        //遮罩
+        (function () {
+            var Mask = require('../modules/m-mask');
+            var mask = new Mask({
+                callback: {
+                    click: function click() {
+                        mask.moduleDomHide();
+                    }
+                }
+            });
+            //mask.moduleDomShow();
+        })();
+        //单选开关
+        (function () {
+            var Radio = require('../modules/m-radio-switch');
+            new Radio({
+                wrap: '.page-radio-switch',
+                callback: {
+                    click: function click(obj) {
+                        console.log(obj);
+                    }
+                }
+            });
+        })();
+        //表格
+        (function () {
+            var Table = require('../modules/m-table');
+            var table = new Table({
+                wrap: ".page-table",
+                data: {
+                    header: [{
+                        html: "<div>header0</div>"
+                    }, {
+                        html: "<div>header1</div>"
+                    }, {
+                        html: "<div>header2</div>"
+                    }],
+                    body: [[{
+                        html: "<div>body0-0</div>"
+                    }, {
+                        html: "<div>body1-0</div>"
+                    }, {
+                        html: "<div>body2-0</div>"
+                    }], [{
+                        html: "<div>body0-1</div>"
+                    }, {
+                        html: "<div>body1-1</div>"
+                    }, {
+                        html: "<div>body2-1</div>"
+                    }], [{
+                        html: "<div>body0-2</div>"
+                    }, {
+                        html: "<div>body1-2</div>"
+                    }, {
+                        html: "<div>body2-2</div>"
+                    }]],
+                    footer: ""
+                }
+            });
+        })();
+        //验证
+        (function () {
+            var ValidateInput = require('../modules/m-validate-form');
+            var aInput = [].slice.call(document.querySelectorAll('.m-validate-form'));
+            aInput.forEach(function (v) {
+                v.validate = new ValidateInput({ form: v });
+                v.validate.validateEventBlur();
+                //v.validate.validateSave();
+            });
+        })();
+        //星评
+        (function () {
+            var Star = require('../modules/m-star');
+            var star = new Star({
+                wrap: ".page-star",
+                callback: {
+                    click: function click(json) {
+                        console.log(json);
+                    }
+                }
+            });
+        })();
+        //每个页面都要用到的js
+        require('../common/common');
     }, { "../base/base": 1, "../common/common": 3, "../modules/m-dialog": 26, "../modules/m-go-top": 28, "../modules/m-loading": 30, "../modules/m-mask": 31, "../modules/m-no-data": 32, "../modules/m-pagination": 33, "../modules/m-radio-switch": 34, "../modules/m-star": 35, "../modules/m-sub-type": 37, "../modules/m-sub-type-es6": 36, "../modules/m-super-type": 39, "../modules/m-super-type-es6": 38, "../modules/m-table": 40, "../modules/m-validate-form": 41 }], 3: [function (require, module, exports) {
         //版权
         (function () {
