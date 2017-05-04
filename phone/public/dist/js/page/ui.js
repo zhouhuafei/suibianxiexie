e175909c4fd59f59ab5ad5aae8ac591a218ef51f
zhouhuafei <zhouhuafei@shopex.cn>
2017-05-04 18:02:14 +0800

    extend优化

--- phone/public/dist/js/page/ui.js
+++ phone/public/dist/js/page/ui.js
@@ -428,30 +428,16 @@ function _inherits(subClass, superClass) { if (typeof superClass !== "function" 
             var inheritsType = Object.prototype.toString.call(opt.inherits).slice(8, -1).toLowerCase();
             if (defaultsType == inheritsType && opt.isDeep) {
                 if (defaultsType == 'object' || defaultsType == 'array') {
+                    //当为对象或者为数组
                     for (var attr in opt.inherits) {
                         if (opt.inherits.hasOwnProperty(attr)) {
                             var attrDefaultsType = Object.prototype.toString.call(opt.defaults[attr]).slice(8, -1).toLowerCase();
                             var attrInheritsType = Object.prototype.toString.call(opt.inherits[attr]).slice(8, -1).toLowerCase();
                             if (attrDefaultsType == attrInheritsType && opt.isDeep) {
                                 //类型相同
-                                if (attrDefaultsType == 'object') {
-                                    //当为对象
+                                if (attrDefaultsType == 'object' || attrDefaultsType == 'array') {
+                                    //当为对象或者为数组
                                     extend({ defaults: opt.defaults[attr], inherits: opt.inherits[attr] });
-                                } else if (attrDefaultsType == 'array') {
-                                    //当为数组时
-                                    opt.inherits[attr].forEach(function (v, i) {
-                                        var vDefaultsType = Object.prototype.toString.call(opt.defaults[attr][i]).slice(8, -1).toLowerCase();
-                                        var vInheritsType = Object.prototype.toString.call(opt.inherits[attr][i]).slice(8, -1).toLowerCase();
-                                        if (vInheritsType == vDefaultsType && opt.isDeep) {
-                                            if (vDefaultsType == 'object') {
-                                                extend({ defaults: opt.defaults[attr][i], inherits: opt.inherits[attr][i] });
-                                            } else {
-                                                opt.defaults[attr][i] = opt.inherits[attr][i];
-                                            }
-                                        } else {
-                                            opt.defaults[attr][i] = opt.inherits[attr][i];
-                                        }
-                                    });
                                 } else {
                                     opt.defaults[attr] = opt.inherits[attr];
                                 }
@@ -490,15 +476,40 @@ function _inherits(subClass, superClass) { if (typeof superClass !== "function" 
         //         }
         //     }
         // });
-        // console.log(obj1);//{ a: 0, b: { b1: 'b1', b2: 1, b3: { c1: 'c1', c2: 2 } } }
+        // console.log(obj1);//{a: 0, b: {b1: 'b1', b2: 1, b3: {c1: 'c1', c2: 2}}}
         // var obj2 = extend({
         //     defaults: {
+        //         a: [
+        //             0,
+        //             [9, 8, 7],
+        //             {
+        //                 arr: [
+        //                     1,
+        //                     2,
+        //                     3,
+        //                     [7, 9, 10],
+        //                     {good: 'good'}
+        //                 ]
+        //             }
+        //         ],
         //         b: [
         //             {a1: 'a1'},
         //             {a2: 'a2'}
         //         ]
         //     },
         //     inherits: {
+        //         a: [
+        //             1,
+        //             [3, 1],
+        //             {
+        //                 arr: [
+        //                     8,
+        //                     8,
+        //                     8,
+        //                     [6, 8]
+        //                 ]
+        //             }
+        //         ],
         //         b: [
         //             'what?',
         //             {b1: 'b1'},
@@ -506,7 +517,7 @@ function _inherits(subClass, superClass) { if (typeof superClass !== "function" 
         //         ]
         //     }
         // });
-        // console.log(obj2);//{ b: [ 'what?', { a2: 'a2', b1: 'b1' }, { b2: 'b2' } ] }
+        // console.log(obj2);//{a: [1, [3, 1, 7],{arr: [8, 8, 8, [6, 8, 10], {good: 'good'}]}], b: ['what?', {a2: 'a2', b1: 'b1'}, {b2: 'b2'}]}
         module.exports = extend;
     }, {}], 9: [function (require, module, exports) {
         //对象的扩展方法
