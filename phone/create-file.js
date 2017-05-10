50776c3cc0e0b5c7fb8167ebf6532d9e6b11b064
zhouhuafei <zhouhuafei@shopex.cn>
2017-05-10 18:52:55 +0800

    创建文件待续。。

--- phone/create-file.js
+++ phone/create-file.js
@@ -9,44 +9,63 @@ class Path {
     }
 }
 const path = new Path();//路径
-//extname扩展名  content内容
-//0.到指定目录去查找指定的文件
-//1.找不到就进行创建
-
-console.log(path.htmlPath + fileName + '.html');
-
 
 var extend = require('./public/dev/js/function/extend');
+
 function CreateFile(json) {
     var opts = extend({
         defaults: {
-            path: '',//路径
-            fileName: '',//文件名
-            extname: ''//扩展名
+            config: {
+                isCover: false,//是否覆盖已有文件
+            },
+            data: {
+                path: '',//路径
+                fileName: '',//文件名
+                extendName: ''//扩展名
+            }
         },
         inherits: json
     });
-    var path = opts.path;
-    var fileName = opts.fileName;
-    var extname = opts.extname;
+    this.init();
+}
+CreateFile.prototype.init=function () {
+    var data = opts.data;
+    var path = data.path;
+    var fileName = data.fileName;
+    var extendName = data.extendName;
+    var file = path + fileName + extendName;
     if (!path) {
         console.log('路径不存在');
     }
     if (!fileName) {
         console.log('文件名不存在');
     }
-    if (!extname) {
+    if (!extendName) {
         console.log('扩展名不存在');
     }
-    fs.readFile(path + fileName + extname, function (err, res) {
-        console.log(err, res);
-        if (res) {
-            console.log('文件已经存在,文件路径:');
+    fs.readFile(file, function (err, res) {
+        if (opts.config.isCover) {
+
         } else {
-            //fs.writeFile(`${dirPath + fileName}.${v[0]}`, `${v[1]}`, function (error) {
-            //    console.log(`文件创建成功,文件路径:`);
-            //});
+            if (res) {
+                console.log(`文件已经存在,文件路径:${file}`);
+            } else {
+                fs.writeFile(file, `123`, function (err) {
+                    if (err) {
+                        console.log(`错误信息:${err}`);
+                    } else {
+                        console.log(`文件创建成功,文件路径:${file}`);
+                    }
+                });
+            }
         }
     });
-}
-CreateFile({path:'',fileName:'',extname:''});
+};
+CreateFile.prototype.writeFile=function () {
+
+};
+new CreateFile({
+    data: {
+        path: `${__dirname}/`, fileName: 'haha', extendName: '.txt'
+    }
+});
