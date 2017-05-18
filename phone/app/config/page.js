3c73597ea00f3834853b486b44185fa3245f146e
zhouhuafei <1123486116@qq.com>
2017-05-18 23:24:23 +0800

    j

--- phone/app/config/page.js
+++ phone/app/config/page.js
@@ -1,47 +1,59 @@
 module.exports = {
     //开发-列表页
-    "dev-list": {
+    'dev-list': {
         route: '/dev-list',//路由
         view: 'dev-list',//视图
         title: 'dev-list'//标题
     },
     //开发-全局页
-    "dev-global": {
+    'dev-global': {
         route: '/dev-global',//路由
         view: 'dev-global',//视图
         title: 'g-global'//标题
     },
     //开发-模块页
-    "dev-module": {
+    'dev-module': {
         route: '/dev-module',//路由
         view: 'dev-module',//视图
         title: 'm-module'//标题
     },
     //开发-标准词汇页
-    "dev-word": {
+    'dev-word': {
         route: '/dev-word',//路由
         view: 'dev-word',//视图
         title: 'dev-word'//标题
     },
     //我的页面
-    "mine": {
+    'mine': {
         route: '/mine',//路由
         view: 'mine',//视图
         title: '我的'//标题
     },
+    //注册页
+    'register': {
+        route: '/register',//路由
+        view: 'register',//视图
+        title: '注册页'//标题
+    },
+    //登陆页
+    'login': {
+        route: '/login',//路由
+        view: 'login',//视图
+        title: '登陆页'//标题
+    },
     //首页
-    "home": {
+    'home': {
         route: '/',//路由
         view: 'home',//视图
         title: '首页'//标题
     },
-    //500
-    "500": {
+    //服务器报错500页面
+    '500': {
         view: '500',//视图
         title: '服务器报错'//标题
     },
-    //404
-    "404": {
+    //服务器报错404页面
+    '404': {
         view: '404',//视图
         title: '页面没有找到'//标题
     }
