7e635400a9714a555c6de30bd0d1c2b8ed263013
zhouhuafei <zhouhuafei@shopex.cn>
2017-06-14 14:09:00 +0800

    route

--- route/phone/route.js
+++ route/phone/route.js
@@ -25,7 +25,7 @@ class Route {
         var controllerPath = `../../controller/phone/`;
         //首页
         app.get(config.home.route, function (req, res) {
-            var Controller = require(`../../controller/phone/home`);//引入文件,路径是相对于这个文件本身
+            var Controller = require(`${controllerPath}home`);//引入文件,路径是相对于这个文件本身
             new Controller({req: req, res: res}).render();
         });
     }
