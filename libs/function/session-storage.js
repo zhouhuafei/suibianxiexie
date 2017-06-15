2f558dcafd5d517e3bce2b39b219b6f0a289e03d
zhouhuafei <1123486116@qq.com>
2017-06-15 23:26:27 +0800

    jjj

---
+++ libs/function/session-storage.js
@@ -0,0 +1,16 @@
+//是否存在
+function isHave(name) {
+}
+
+//存数据
+function postData(name, data) {
+}
+
+//取数据
+function getData(name) {
+
+}
+
+module.exports.isHave = isHave;
+module.exports.postData = postData;
+module.exports.getData = getData;
