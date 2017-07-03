51ec4eea6dc328ac9a3f759bfc93f0e6bf38efa1
zhouhuafei <zhouhuafei@shopex.cn>
2017-07-03 17:16:34 +0800

    es6解析失败

--- webpack.config.js
+++ webpack.config.js
@@ -25,19 +25,26 @@ class ConfigPath {
 const configPath = new ConfigPath();//配置路径
 //环境----是否是生产环境(默认是开发环境)
 let productionConfig = {
-    hash: '',
-    chunkhash: '',
-    contenthash: '',
-    min: '',
-    watch: true,
+    hash: '',//图片和字体用到了这个hash
+    chunkhash: '',//js用到了这个chunkhash
+    contenthash: '',//css用到了这个contenthash
+    min: '',//第三方库是否引用压缩版(生产环境引用压缩版)
+    isMinCss: false,//是否压缩css
+    isWatch: true,//是否监听
+    minView: {},//压缩视图模板文件
 };
 if (isProduction) {
     productionConfig = {
-        hash: '[hash:8].',
-        chunkhash: '[chunkhash].',
-        contenthash: '[contenthash].',
-        min: 'min.',
-        watch: true,
+        hash: '[hash:8].',//图片和字体用到了这个hash
+        chunkhash: '[chunkhash].',//js用到了这个chunkhash
+        contenthash: '[contenthash].',//css用到了这个contenthash
+        min: 'min.',//第三方库是否引用压缩版(生产环境引用压缩版)
+        isMinCss: true,//是否压缩css
+        isWatch: true,//是否监听
+        minView: {
+            removeComments: true, //移除HTML中的注释
+            collapseWhitespace: false //删除空白符与换行符
+        }
     };
 }
 //插件----集合
@@ -49,6 +56,10 @@ let plugins = [
     //插件----把每个入口都有用到的js和css分别提取为this-is-global-file.js和this-is-global-file.css
     new webpack.optimize.CommonsChunkPlugin({name: 'this-is-global-file'})
 ];
+//插件----压缩js
+if (isProduction) {
+    plugins.push(new webpack.optimize.UglifyJsPlugin({compress: {warnings: false}}));
+}
 //插件----处理视图模板页面文件
 let allPageHtml = fs.readdirSync(`${configPath.viewEntryPath}pages/`);
 allPageHtml.forEach(function (v) {
@@ -59,6 +70,7 @@ allPageHtml.forEach(function (v) {
             filename: `${configPath.viewOutputPath}pages/${v}`,//文件名
             favicon: `${configPath.imagesEntryPath}partials/favicon.ico`,//网站的icon图标
             chunks: ['this-is-global-file', fileName],//需要引入的chunk，不配置就会引入所有页面的资源
+            minify: productionConfig.minView//压缩视图模板文件
         })
     );
 });
@@ -69,7 +81,8 @@ allPartialsHtml.forEach(function (v) {
         new HtmlWebpackPlugin({
             template: `${configPath.viewEntryPath}partials/${v}`,//模板
             filename: `${configPath.viewOutputPath}partials/${v}`,//文件名
-            inject: false
+            inject: false,
+            minify: productionConfig.minView//压缩视图模板文件
         })
     );
 });
@@ -104,7 +117,7 @@ let webpackConfig = {
     //插件----配置
     plugins: plugins,
     //监听----配置
-    watch: productionConfig.watch,
+    watch: productionConfig.isWatch,
     //模块----模块加载相关的配置
     module: {
         //rules----loader加载器的规则集合
@@ -115,7 +128,12 @@ let webpackConfig = {
                 exclude: /(node_modules|bower_components)/,
                 use: ExtractTextPlugin.extract({
                     fallback: 'style-loader',
-                    use: ['css-loader', 'postcss-loader', 'sass-loader']
+                    use: [{
+                        loader: 'css-loader',
+                        options: {
+                            minimize: productionConfig.isMinCss //css压缩
+                        }
+                    }, 'postcss-loader', 'sass-loader']
                 })
             },
             //loader----es6转成es5
