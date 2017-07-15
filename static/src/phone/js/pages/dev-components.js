edbd40d76afee917a3cab0b7cc9cbadc2f2bc125
zhouhuafei <1123486116@qq.com>
2017-07-15 14:58:50 +0800

    解放弹窗

--- static/src/phone/js/pages/dev-components.js
+++ static/src/phone/js/pages/dev-components.js
@@ -100,22 +100,22 @@ window.addEventListener('load', function () {
         //弹窗测试
         (function () {
             var Dialog = require('../components/g-dialog');
-            // new Dialog({
-            //     callback: {
-            //         confirm: function () {
-            //             new Dialog({config: {alert: {icon: 'icon-chenggong', content: '已确认'}}});
-            //         },
-            //         cancel: function () {
-            //             new Dialog({config: {alert: {icon: 'icon-chenggong', content: '已取消'}}});
-            //         },
-            //         close: function () {
-            //             new Dialog({config: {alert: {icon: 'icon-chenggong', content: '已关闭'}}});
-            //         }
-            //     },
-            //     config: {
-            //         type: 'confirm'
-            //     }
-            // });
+            new Dialog({
+                callback: {
+                    confirm: function () {
+                        new Dialog({config: {alert: {icon: 'icon-chenggong', content: '已确认'}}});
+                    },
+                    cancel: function () {
+                        new Dialog({config: {alert: {icon: 'icon-chenggong', content: '已取消'}}});
+                    },
+                    close: function () {
+                        new Dialog({config: {alert: {icon: 'icon-chenggong', content: '已关闭'}}});
+                    }
+                },
+                config: {
+                    type: 'confirm'
+                }
+            });
         })();
 
         //分页测试
