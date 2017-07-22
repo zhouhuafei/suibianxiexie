323994506d739d1ab5c2c5380e2a3098205d3f8e
zhouhuafei <1123486116@qq.com>
2017-07-22 16:33:45 +0800

    重置密码与找回密码

---
+++ controllers/phone/pages/password-find.js
@@ -0,0 +1,19 @@
+//忘记密码,去找回,页面路由的控制器
+let Super = require('./super');//超类型
+
+class PasswordFind extends Super {
+    constructor(json) {
+        super(json);
+        this.fileName = this.path.basename(__filename, '.js');//覆盖超类型的属性
+        this.initData();//调用超类型的初始化数据
+        this.handleData();//处理数据pageInfo
+    }
+
+    handleData() {
+        let req = this.opts.req;
+        let query = req.query;
+        //pageInfo数据处理待续...
+    }
+}
+
+module.exports = PasswordFind;
---
+++ controllers/phone/pages/password-reset.js
@@ -0,0 +1,19 @@
+//修改密码,去重置,页面路由的控制器
+let Super = require('./super');//超类型
+
+class PasswordReset extends Super {
+    constructor(json) {
+        super(json);
+        this.fileName = this.path.basename(__filename, '.js');//覆盖超类型的属性
+        this.initData();//调用超类型的初始化数据
+        this.handleData();//处理数据pageInfo
+    }
+
+    handleData() {
+        let req = this.opts.req;
+        let query = req.query;
+        //pageInfo数据处理待续...
+    }
+}
+
+module.exports = PasswordReset;
