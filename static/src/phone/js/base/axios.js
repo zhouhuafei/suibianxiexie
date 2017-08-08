f81f8a362367d37072513b4d5d5814cdeaf9b5f0
zhouhuafei <zhouhuafei@shopex.cn>
2017-08-08 20:36:53 +0800

    axioshbs

--- static/src/phone/js/base/axios.js
+++ static/src/phone/js/base/axios.js
@@ -1,7 +1,7 @@
 let axios = require('axios');
 let tools = require('./tools');
 
-export default function (json) {
+module.exports = function (json) {
     let opts = tools.extend({
         defaults: {},
         inherits: json
