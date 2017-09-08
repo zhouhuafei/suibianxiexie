6511bab30d7fcf2ec81112ef11b8fb4b410d09f9
zhouhuafei <zhouhuafei@shopex.cn>
2017-09-08 18:23:40 +0800

    验证码待续

--- schemas/phone/user.js
+++ schemas/phone/user.js
@@ -1,10 +1,22 @@
 const mongoose = require('../../config/mongoose');
 
 const User = mongoose.Schema({
-    username: String,
-    password: String,
-    create_time: Date,
-    update_time: Date,
+    username: {
+        type: String,
+        default: null,
+    },
+    password: {
+        type: String,
+        default: null,
+    },
+    create_time: {
+        type: String,
+        default: null,
+    },
+    update_time: {
+        type: String,
+        default: null,
+    },
 });
 
 module.exports = mongoose.model('User', User);
