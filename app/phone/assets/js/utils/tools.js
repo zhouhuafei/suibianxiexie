aebf07a680abe4b600f891d2c62bb786556ee7ef
zhouhuafei <zhouhuafei@shopex.cn>
2017-09-19 18:02:20 +0800

    tools

--- app/phone/assets/js/utils/tools.js
+++ app/phone/assets/js/utils/tools.js
@@ -276,4 +276,26 @@ Tools.prototype.isEmail = function (value) {
     return reg.test(value);
 };
 
+// {a:1,b:2} 序列成 'a=1&b=2'
+Tools.prototype.queryStringify = function (json) {
+    const obj = json || {};
+    const result = [];
+    Object.keys(obj).forEach(function (key) {
+        result.push(`${key}=${obj[key]}`);
+    });
+    return result.join('&');
+};
+
+// 'a=1&b=2' 解析成 {a:1,b:2}
+Tools.prototype.queryParse = function (str) {
+    const result = {};
+    if (str) {
+        str.split('&').forEach(function (v) {
+            const arr = v.split('=');
+            result[arr[0]] = arr[1];
+        });
+    }
+    return result;
+};
+
 module.exports = new Tools();
