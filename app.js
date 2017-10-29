be3c7f854a09df57f509f5009c61a5e2742f8ecb
zhouhuafei <1123486116@qq.com>
2017-10-29 23:36:11 +0800

    error-err-button-btn

--- app.js
+++ app.js
@@ -1,28 +1,37 @@
-// express
+// 环境以及配置
 const env = process.env.NODE_ENV; // 开发环境 or 生产环境
 const isProduction = env !== 'development'; // 是否是生产环境
 const configDb = require('./db/config'); // 数据库配置
 const configRedis = configDb.redis[env]; // redis的配置
 const ms = require('ms'); // 转成毫秒数
-const compression = require('compression'); // gzip压缩
+
+// express应用
 const express = require('express'); // express
-const session = require('express-session'); // session
-const RedisStore = require('connect-redis')(session); // session存redis
-const cookieParser = require('cookie-parser'); // cookie数据解析
-const bodyParser = require('body-parser'); // post数据解析
 const app = express(); // app
-const secret = 'suibianxiexie'; // sessionID cookie的密钥
+
+// gzip压缩
+const compression = require('compression'); // gzip压缩
 app.use(compression());// gzip压缩
+
+// 静态资源
 if (isProduction) {
     app.use(express.static('dist/assets', {maxAge: ms('1y')})); // 托管资源文件(一年缓存)
 } else {
     app.use(express.static('dist/assets')); // 托管资源文件(无缓存)
 }
 app.use(express.static('dist-no-delete-assets-no-cache')); // 托管资源文件(无缓存)
+
+// 数据解析
+const bodyParser = require('body-parser'); // 可以对post delete update请求方式进行数据解析
 app.use(bodyParser.urlencoded({extended: false})); // parse application/x-www-form-urlencoded
 app.use(bodyParser.json()); // parse application/json
+
+// cookie以及session
+const cookieParser = require('cookie-parser'); // cookie数据解析
+const session = require('express-session'); // session
+const RedisStore = require('connect-redis')(session); // session存redis
+const secret = 'suibianxiexie'; // sessionID cookie的密钥
 app.use(cookieParser(secret)); // cookie
-// session
 app.use(session({
     resave: false, // 是指每次请求都重新设置session cookie，假设你的cookie是10分钟过期，每次请求都会再设置10分钟
     saveUninitialized: false, // 是指无论有没有session cookie，每次请求都设置个session cookie ，默认给个标示为 connect.sid
@@ -38,11 +47,11 @@ const ejs = require('ejs');
 app.set('views', 'dist/views/');
 app.set('view engine', 'ejs');
 
-// pc的路由待续...
-// phone的路由
+// 路由(phone-pages)
 const RoutePhonePages = require('./app/phone/routes/pages/route');
 new RoutePhonePages({app: app});
-const RoutePhoneApi = require('./app/phone/routes/api/route');
+// 路由(phone-api)
+const RoutePhoneApi = require('./app/phone/routes/api/route'); // phone-api的路由
 new RoutePhoneApi({app: app});
 
 // 404
@@ -51,7 +60,7 @@ app.use(function (req, res, next) {
 });
 
 // 500
-app.use(function (err, req, res) {
+app.use(function (err, req, res, next) {
     if (err) {
         res.status(500).send('500 - server error');
     }
@@ -65,6 +74,7 @@ mongoose.connection.on('connected', function () {
     redisClient.on('connect', function () {
         // 把redis的客户端应用到全局到app上使用
         app.redisClient = redisClient;
+
         // 监听端口
         const server = app.listen('5555', function () {
             console.log('server connection open to:\n', `http://localhost:${server.address().port}`);
