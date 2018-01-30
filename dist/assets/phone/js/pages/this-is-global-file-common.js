7617dbafacc1a613a9e2be93a4da0c6d449cb54b
zhouhuafei <1123486116@qq.com>
2018-01-30 21:45:03 +0800

    dev

--- dist/assets/phone/js/pages/this-is-global-file-common.js
+++ dist/assets/phone/js/pages/this-is-global-file-common.js
@@ -3290,7 +3290,9 @@ var Sub = tools.constructorInherit(Super, {
                     self.mask = new Mask(self.opts.config.mask);
                 }
                 if (self.wrapDom && getComputedStyle(self.wrapDom).position === 'static') {
-                    self.wrapDom.style.position = 'relative';
+                    if (self.wrapDom.style.position === '' || self.wrapDom.style.position === 'static') {
+                        self.wrapDom.style.position = 'relative';
+                    }
                 }
             }
         },
@@ -7874,7 +7876,9 @@ var Sub = tools.constructorInherit(Super, {
         click: function click() {},
         moduleDomRenderBefore: function moduleDomRenderBefore(self) {
             if (self.wrapDom && getComputedStyle(self.wrapDom).position === 'static') {
-                self.wrapDom.style.position = 'relative';
+                if (self.wrapDom.style.position === '' || self.wrapDom.style.position === 'static') {
+                    self.wrapDom.style.position = 'relative';
+                }
             }
         }
     },
