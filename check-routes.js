d6d85546680a2e0b85ead3e3192de5f1c1aaa3c9
zhouhuafei <zhouhuafei@shopex.cn>
2018-02-22 16:47:43 +0800

    favicon.ico

---
+++ check-routes.js
@@ -0,0 +1,8 @@
+// 检查路由对应的文件是否全部存在
+const routesFilePath = [
+    `${__dirname}/app/phone/routes/api/`,
+    `${__dirname}/app/phone/routes/pages/`,
+    `${__dirname}/app/admin/routes/pages/`,
+    `${__dirname}/app/admin/routes/pages/`,
+];
+const routesConfig = {};
