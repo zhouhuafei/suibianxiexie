f89a6da60ebb3f24896a3c8c83eb97d2d60689d7
zhouhuafei <zhouhuafei@shopex.cn>
2018-02-22 11:50:59 +0800

    url

--- app/phone/assets/js/pages/home.js
+++ app/phone/assets/js/pages/home.js
@@ -4,6 +4,9 @@ const Super = require('../pages-super/super');
 class Sub extends Super {
     power() {
         const superSelf = this;
+        const dataInfo = superSelf.dataInfo;
+        const routes = dataInfo.routes;
+
         // slide切换
         (function () {
             const Slide = require('../components-dom/g-slide');
@@ -59,7 +62,43 @@ class Sub extends Super {
         // 导航
         (function () {
             const Navigation = require('../components-dom/g-navigation');
-            new Navigation({wrap: '.page-navigation'});
+            new Navigation({
+                wrap: '.page-navigation',
+                data: {
+                    items: [
+                        {
+                            href: routes['home'].route,
+                            icon: 'icon-home',
+                            text: '首页',
+                            isShowMark: false,
+                        },
+                        {
+                            href: routes['dev-globals'].route,
+                            icon: 'icon-dev',
+                            text: '开发全局',
+                            isShowMark: false,
+                        },
+                        {
+                            href: routes['dev-components'].route,
+                            icon: 'icon-dev',
+                            text: '开发组件',
+                            isShowMark: false,
+                        },
+                        {
+                            href: routes['dev-words'].route,
+                            icon: 'icon-dev',
+                            text: '开发词汇',
+                            isShowMark: false,
+                        },
+                        {
+                            href: routes['mine'].route,
+                            icon: 'icon-mine',
+                            text: '我的',
+                            isShowMark: false,
+                        },
+                    ],
+                },
+            });
         }());
 
         // vue
@@ -95,8 +134,8 @@ class Sub extends Super {
             data: {
                 isJsonp: true,
             },
-            callback: function (dadaInfo) {
-                console.log('jsonp error test:\n', dadaInfo);
+            callback: function (dataInfo) {
+                console.log('jsonp error test:\n', dataInfo);
             },
         });
         */
@@ -106,8 +145,8 @@ class Sub extends Super {
         this.axios({
             url: '/phone/api/verify-code-register-random2/',
             method: 'get',
-        }).then(function (dadaInfo) {
-            console.log('axios error test:\n', dadaInfo);
+        }).then(function (dataInfo) {
+            console.log('axios error test:\n', dataInfo);
         });
         */
     }
