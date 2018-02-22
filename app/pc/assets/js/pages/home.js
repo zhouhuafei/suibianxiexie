bdbbecc34739b586aec110f2e8ee7c7cb56dbe02
zhouhuafei <zhouhuafei@shopex.cn>
2018-02-22 19:07:41 +0800

    tools.extend.phone

--- app/pc/assets/js/pages/home.js
+++ app/pc/assets/js/pages/home.js
@@ -6,48 +6,6 @@ class Sub extends Super {
         const superSelf = this;
         const dataInfo = superSelf.dataInfo;
         const routes = dataInfo.routes;
-
-        // 导航
-        (function () {
-            const Navigation = require('../components-dom/g-navigation');
-            new Navigation({
-                wrap: '.page-navigation',
-                data: {
-                    items: [
-                        {
-                            href: routes['home'].route,
-                            icon: 'icon-home',
-                            text: '首页',
-                            isShowMark: false,
-                        },
-                        {
-                            href: routes['dev-globals'].route,
-                            icon: 'icon-dev',
-                            text: '开发全局',
-                            isShowMark: false,
-                        },
-                        {
-                            href: routes['dev-components'].route,
-                            icon: 'icon-dev',
-                            text: '开发组件',
-                            isShowMark: false,
-                        },
-                        {
-                            href: routes['dev-words'].route,
-                            icon: 'icon-dev',
-                            text: '开发词汇',
-                            isShowMark: false,
-                        },
-                        {
-                            href: routes['mine'].route,
-                            icon: 'icon-mine',
-                            text: '我的',
-                            isShowMark: false,
-                        },
-                    ],
-                },
-            });
-        }());
     }
 }
 
