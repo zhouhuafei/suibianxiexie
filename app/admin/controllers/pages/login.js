bf73495b0db81f37aeea635e7299026f6b0395fe
zhouhuafei <1123486116@qq.com>
2018-04-27 08:21:08 +0800

    extends

--- app/admin/controllers/pages/login.js
+++ app/admin/controllers/pages/login.js
@@ -1,4 +1,5 @@
-const Super = require('../pages-super/super'); // 超类型
+// const Super = require('../pages-super/super'); // 超类型
+const Super = require('./register'); // 超类型
 
 class Sub extends Super {
 }
--- app/admin/controllers/pages/password-modify.js
+++ app/admin/controllers/pages/password-modify.js
@@ -1,22 +1,7 @@
-const Super = require('../pages-super/super'); // 超类型
+// const Super = require('../pages-super/super'); // 超类型
+const Super = require('./register'); // 超类型
 
 class Sub extends Super {
-    handleData() {
-        const self = this;
-        const tools = self.tools; // 工具方法集合
-        const opts = self.opts;
-        const req = opts.req;
-        const session = req.session;
-        const data = req.data;
-        const dataInfo = self.dataInfo;
-        const page = dataInfo.page;
-        const fnVerifyCodeCanvas = require('../../../../utils/verify-code-canvas'); // 验证码,图文随机
-        const verifyCodeCanvas = fnVerifyCodeCanvas();
-        session.adminVerifyCodeCanvas = verifyCodeCanvas.text;
-        delete verifyCodeCanvas.text;
-        page.verifyCodeCanvas = verifyCodeCanvas;
-        self.render(); // 渲染视图(渲染数据)
-    }
 }
 
 module.exports = Sub;
