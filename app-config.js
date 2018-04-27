5ee9a3ab0895138a35e0cbd49e7834c25844c7db
zhouhuafei <zhouhuafei@shopex.cn>
2018-04-27 13:02:50 +0800

    config

--- app-config.js
+++ app-config.js
@@ -4,8 +4,9 @@ class Config {
         this.adminDirectory = `${__dirname}/admin/`; // admin的目录
         this.phoneDirectory = `${__dirname}/phone/`; // phone的目录
         this.pcDirectory = `${__dirname}/pc/`; // pc的目录
-        this.staticCacheDirectory = `${__dirname}dist/`; // 静态缓存资源目录
-        this.staticNoCacheDirectory = `${__dirname}dist-no-delete-assets-no-cache/`; // 静态非缓存资源目录
+        this.staticCacheDirectory = `${__dirname}/dist/`; // 静态缓存资源目录
+        this.staticNoCacheDirectory = `${__dirname}/dist-no-delete-assets-no-cache/`; // 静态非缓存资源目录
+        this.utilsDirectory = `${__dirname}/utils/`; // utils目录
         this.isEnabledSingleDeviceLogin = false; // 是否启用单设备登录
     }
 }
