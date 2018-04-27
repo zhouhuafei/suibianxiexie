3c76376a978bac87c0df9ccd4d0990b7a6d4445c
zhouhuafei <zhouhuafei@shopex.cn>
2018-04-27 16:40:28 +0800

    admins

--- app/admin/models/mongoose/admins.js
+++ app/admin/models/mongoose/admins.js
@@ -1,4 +1,6 @@
 const mongoose = require('../../../../db/mongoose');
+
+// 数据格式
 const schema = new mongoose.Schema({
     // 用户名(账号)
     username: {
@@ -24,26 +26,20 @@ const schema = new mongoose.Schema({
         },
     },
 });
-const bcrypt = require('bcrypt'); // 加密工具
-const saltStrength = 10; // 加密强度
+
 // 使用pre中间件在用户信息存储前进行密码加密
+const fnBcrypt = require('../../../../utils/bcrypt'); // 加密工具
 const saltPassword = function (next) {
     const self = this;
     // 当密码有更改或者是新密码的时候
     if (self.isModified('password')) {
         // 进行加密(加盐)
-        bcrypt.genSalt(saltStrength, function (error, salt) {
+        fnBcrypt(self.password, function (error, hash) {
             if (error) {
                 next(error);
             } else {
-                bcrypt.hash(self.password, salt, function (error, hash) {
-                    if (error) {
-                        next(error);
-                    } else {
-                        self.password = hash;
-                        next();
-                    }
-                });
+                self.password = hash;
+                next();
             }
         });
     } else {
@@ -53,6 +49,7 @@ const saltPassword = function (next) {
 schema.pre('save', saltPassword); // 更新密码不会触发这个，所以更新密码时，需要重新加密。
 
 // 对比密码
+const bcrypt = require('bcrypt');
 schema.methods.comparePassword = function (candidatePassword, callback) {
     bcrypt.compare(candidatePassword, this.password, function (error, isMatch) {
         if (error) {
