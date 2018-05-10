20de535356d5e9552d81c3a538b83b27b47c6012
zhouhuafei <zhouhuafei@shopex.cn>
2018-05-10 18:38:37 +0800

    admin

--- app/admin/assets/js/pages/website-info.js
+++ app/admin/assets/js/pages/website-info.js
@@ -46,6 +46,20 @@ class Sub extends Super {
             }).then(function (json) {
                 console.log('测试formData2', json);
             });
+
+            // 测试
+            const formData3 = new FormData();
+            formData3.append('', files[0]);
+            formData3.append(null, files[0]);
+            formData3.append(undefined, files[0]);
+            formData3.append('list', '789');
+            axios({
+                url: dataInfo.api.list.route,
+                method: 'post',
+                data: formData3,
+            }).then(function (json) {
+                console.log('测试formData3', json);
+            });
         });
     }
 }
--- app/admin/controllers/api/list.js
+++ app/admin/controllers/api/list.js
@@ -3,6 +3,16 @@ const Super = require('../api-super/super'); // 超类型
 class Sub extends Super {
     // (增)(覆)获取数据(覆盖超类型)
     postData() {
+        const self = this;
+        const tools = self.tools; // 工具方法集合
+        const opts = self.opts;
+        const req = opts.req;
+        const data = req.data;
+        self.render({
+            status: 'success',
+            message: '上传成功',
+            result: {data: [data, req.files, req.file]},
+        });
         this.render({
             status: 'success',
             message: '成功',
