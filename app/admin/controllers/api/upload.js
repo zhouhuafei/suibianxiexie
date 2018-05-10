f19ac55b30c7572953c8392176a2f7f1734f084e
zhouhuafei <zhouhuafei@shopex.cn>
2018-05-10 11:33:53 +0800

    wrap

--- app/admin/controllers/api/upload.js
+++ app/admin/controllers/api/upload.js
@@ -1,8 +1,8 @@
 const Super = require('../api-super/super'); // 超类型
-const Uploads = require('../../models/mongoose/uploads');
-const multiparter = require('multiparty');
 const appConfig = require('../../../../app-config');
-const form = new multiparter.Form({uploadDir: `${appConfig.projectDir}static-cache-wrap/static-cache/upload/admin/`});
+const Uploads = require('../../models/mongoose/uploads');
+const multer = require('multer');
+const uploadMulter = multer({dest: `${appConfig.projectDir}static-cache-wrap/static-cache/upload/admin/`});
 
 class Sub extends Super {
     // (增)(覆)获取数据(覆盖超类型)
@@ -11,13 +11,12 @@ class Sub extends Super {
         const tools = self.tools; // 工具方法集合
         const opts = self.opts;
         const req = opts.req;
+        const res = opts.res;
         const data = req.data;
-        form.parse(req, parse);
-
-        function parse(error, fields, files) {
+        uploadMulter(req, res, function (error, fields, files) {
             if (error) {
                 self.render({
-                    message: 'multiparty模块数据解析(parse)失败',
+                    message: 'multer模块数据解析失败',
                     failureInfo: error,
                 });
             } else {
@@ -28,36 +27,36 @@ class Sub extends Super {
                     message: '上传成功',
                     result: {data: []},
                 });
-                /*
-                const uploads = new Uploads({
-                    name: null,
-                    type: null,
-                    size: {
-                        B: null, // 多少字节
-                        KB: null, // 多少千字节
-                        M: null, // 多少兆
-                    },
-                    url: null,
-                    createTime: null,
-                });
-                uploads.save(function (error, result) {
-                    // 数据库插入出现错误
-                    if (error) {
-                        self.render({
-                            message: '数据库插入出现错误',
-                            failureInfo: error,
-                        });
-                        return;
-                    }
-                    self.render({
-                        status: 'success',
-                        message: '上传成功',
-                        result: {data: []},
-                    });
-                });
-                */
             }
-        }
+        });
+        /*
+        const uploads = new Uploads({
+            name: null,
+            type: null,
+            size: {
+                B: null, // 多少字节
+                KB: null, // 多少千字节
+                M: null, // 多少兆
+            },
+            url: null,
+            createTime: null,
+        });
+        uploads.save(function (error, result) {
+            // 数据库插入出现错误
+            if (error) {
+                self.render({
+                    message: '数据库插入出现错误',
+                    failureInfo: error,
+                });
+                return;
+            }
+            self.render({
+                status: 'success',
+                message: '上传成功',
+                result: {data: []},
+            });
+        });
+        */
     }
 }
 
