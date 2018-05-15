40da125024762fd9470e2b83b0ed112bd7303d57
zhouhuafei <zhouhuafei@shopex.cn>
2018-05-15 12:06:44 +0800

    map

--- app/admin/routes/api/route.js
+++ app/admin/routes/api/route.js
@@ -29,7 +29,7 @@ class Route {
                 (function (Controller, attr) {
                     if (attr === 'galleries') {
                         upload = multer({
-                            dest: `${appConfig.projectDir}static-cache-wrap/static-cache/galleries/admin/`,
+                            dest: `${appConfig.projectDir}static-cache-wrap/static-cache/admin/galleries/`,
                             limits: {
                                 fileSize: 300 * 1024, // 单个文件的大小不能超过300kb。
                                 files: 6, // 每次最多上传6个文件。
