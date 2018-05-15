527dcd6763d40634c14296c4c9592429cc8abe93
zhouhuafei <1123486116@qq.com>
2018-05-15 21:12:37 +0800

    zhushi

--- app/admin/models/mongoose/galleries.js
+++ app/admin/models/mongoose/galleries.js
@@ -5,47 +5,57 @@ const schema = new mongoose.Schema({
     // 文件的mime类型
     mimeType: {
         type: String,
-        required: [true, 'The mimeType of the galleries is required.'],
+        required: true,
+        trim: true,
     },
     // 保存在 destination 中的文件名
     filename: {
         type: String,
-        required: [true, 'The filename of the galleries is required.'],
+        required: true,
+        trim: true,
     },
     // 已上传文件所在的目录
     destination: {
         type: String,
-        required: [true, 'The destination of the galleries is required.'],
+        required: true,
+        trim: true,
     },
     // 已上传文件的完整路径
     path: {
         type: String,
-        required: [true, 'The path of the galleries is required.'],
+        required: true,
+        trim: true,
     },
     // 文件大小，多少字节
     size: {
         type: Number,
-        required: [true, 'The size of the galleries is required.'],
+        required: true,
+        trim: true,
     },
     // 文件在用户计算机上的名称
     originalName: {
         type: String,
-        required: [true, 'The originalName of the galleries is required.'],
+        required: true,
+        trim: true,
     },
     // 文件创建的时间(上传的时间)
     createTime: {
         type: Date,
-        required: [true, 'The createTime of the galleries is required.'],
+        required: true,
+        trim: true,
     },
     // 图片是否被使用
     isUsed: {
         type: Boolean,
         default: false,
+        required: true,
+        trim: true,
     },
     // 图片分类id
     categoryId: {
         type: String,
-        required: [true, 'The categoryId of the galleries is required.'],
+        required: true,
+        trim: true,
     },
 });
 
