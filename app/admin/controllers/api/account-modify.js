bcc8970a7f65eca35a55b6feb90fe7fad81e679b
zhouhuafei <zhouhuafei@shopex.cn>
2018-06-29 18:50:45 +0800

    修改密码和修改账号

--- app/admin/controllers/api/account-modify.js
+++ app/admin/controllers/api/account-modify.js
@@ -9,38 +9,21 @@ class Sub extends Super {
         const opts = self.opts;
         const req = opts.req;
         const session = req.session;
+        const adminInfo = session.adminInfo;
         const data = req.data;
-        const oldUsername = (data['old-username'] || '').trim(); // 旧用户名
         const newUsername = (data['new-username'] || '').trim(); // 新用户名
         const verifyCodeCanvas = (data['verify-code-canvas'] || '').trim(); // 验证码,图文随机
         const sessionVerifyCodeCanvasAdmin = session.verifyCodeCanvasAdmin; // 先保存一份验证码，留着下面做验证。
         delete session.verifyCodeCanvasAdmin; // 请求一次之后就清掉验证码，无论成功失败，都要让验证码无效。
         const checkStr = tools.checkStr;
-        if (checkStr.isEmpty(oldUsername)) {
-            self.render({
-                message: '旧用户名不能为空',
-                result: {'old-username': oldUsername},
-            });
-        } else if (checkStr.isEmpty(newUsername)) {
-            self.render({
-                message: '新用户名不能为空',
-                result: {'old-username': oldUsername},
-            });
-        } else if (newUsername === oldUsername) {
-            self.render({
-                message: '新用户名不能和旧用户名重复',
-                result: {'old-username': oldUsername},
-            });
+        if (checkStr.isEmpty(newUsername)) {
+            self.render({message: '新用户名不能为空'});
+        } else if (newUsername === adminInfo.username) {
+            self.render({message: '新用户名不能和旧用户名重复'});
         } else if (checkStr.isEmpty(verifyCodeCanvas)) {
-            self.render({
-                message: '验证码不能为空',
-                result: {'verify-code-canvas': verifyCodeCanvas},
-            });
+            self.render({message: '验证码不能为空'});
         } else if (verifyCodeCanvas !== sessionVerifyCodeCanvasAdmin) {
-            self.render({
-                message: '验证码错误',
-                result: {'verify-code-canvas': verifyCodeCanvas},
-            });
+            self.render({message: '验证码错误'});
         } else {
             const Admins = require('../../models/mongoose/admins');
             // 先检测新用户名是否已经被注册。
@@ -56,7 +39,6 @@ class Sub extends Super {
                         username: newUsername,
                         loginStamp: `${Math.random()}`.split('.')[1], // 让所有用户掉线
                     };
-                    const adminInfo = session.adminInfo;
                     Admins.update({_id: adminInfo._id}, {$set: $set}, function (error) {
                         // 数据库更新出现错误
                         if (error) {
--- app/admin/controllers/api/password-modify.js
+++ app/admin/controllers/api/password-modify.js
@@ -10,9 +10,8 @@ class Sub extends Super {
         const opts = self.opts;
         const req = opts.req;
         const session = req.session;
+        const adminInfo = session.adminInfo;
         const data = req.data;
-        const oldUsername = (data['old-username'] || '').trim(); // 旧用户名
-        const newUsername = (data['new-username'] || '').trim(); // 新用户名
         const oldPassword = (data['old-password'] || '').trim(); // 旧密码 -> isEmpty方法内部去掉了首尾空格,不适用于验证密码是否为空
         const newPassword = (data['new-password'] || '').trim(); // 新密码 -> isEmpty方法内部去掉了首尾空格,不适用于验证密码是否为空
         const repeatNewPassword = (data['repeat-new-password'] || '').trim(); // 新密码二次确认 -> isEmpty方法内部去掉了首尾空格,不适用于验证密码是否为空
@@ -20,121 +19,75 @@ class Sub extends Super {
         const sessionVerifyCodeCanvasAdmin = session.verifyCodeCanvasAdmin; // 先保存一份验证码，留着下面做验证。
         delete session.verifyCodeCanvasAdmin; // 请求一次之后就清掉验证码，无论成功失败，都要让验证码无效。
         const checkStr = tools.checkStr;
-        if (checkStr.isEmpty(oldUsername)) {
-            self.render({
-                message: '账号不能为空',
-                result: {'old-username': oldUsername},
-            });
-        } else if (oldPassword === '' || newPassword === '' || repeatNewPassword === '') {
-            self.render({
-                message: '密码不能为空',
-                result: {
-                    'old-password': oldPassword,
-                    'new-password': newPassword,
-                    'repeat-new-password': repeatNewPassword,
-                },
-            });
+        if (oldPassword === '') {
+            self.render({message: '旧密码不能为空'});
+        } else if (newPassword === '' || repeatNewPassword === '') {
+            self.render({message: '新密码不能为空'});
+        } else if (repeatNewPassword === '') {
+            self.render({message: '重复新密码不能为空'});
+        } else if (newPassword === oldPassword) {
+            self.render({message: '新密码不能和旧密码一样'});
         } else if (newPassword !== repeatNewPassword) {
-            self.render({
-                message: '两次输入的新密码不一致',
-                result: {
-                    'old-password': oldPassword,
-                    'new-password': newPassword,
-                    'repeat-new-password': repeatNewPassword,
-                },
-            });
+            self.render({message: '两次输入的新密码不一致'});
         } else if (checkStr.isEmpty(verifyCodeCanvas)) {
-            self.render({
-                message: '验证码不能为空',
-                result: {'verify-code-canvas': verifyCodeCanvas},
-            });
+            self.render({message: '验证码不能为空'});
         } else if (verifyCodeCanvas !== sessionVerifyCodeCanvasAdmin) {
-            self.render({
-                message: '验证码错误',
-                result: {'verify-code-canvas': verifyCodeCanvas},
-            });
+            self.render({message: '验证码错误'});
         } else {
             const Admins = require('../../models/mongoose/admins');
-            let callNum = 1;
-            // 新用户名存在且新用户名不等于老用户名，需要调用2次，因为下面会进行查询新密码是否已经存在。
-            if (newUsername !== '' && newUsername !== oldUsername) {
-                callNum = 2;
-            }
-            // 调用callNum次才会触发这个更新数据库的方法。
-            const fnUpdateDB = multipleCalls(callNum, function (json) {
-                const adminInfo = json.data.update;
-                adminInfo.comparePassword(oldPassword, function (error, isMatch) {
-                    if (error) {
-                        self.render({
-                            message: '密码对比出现错误',
-                            failureInfo: error,
-                        });
-                    }
-                    if (isMatch) {
-                        // 用户名和老密码匹配了,则进行密码替换
-                        const fnBcrypt = require('../../../../utils/bcrypt'); // 加密工具
-                        // 密码加密
-                        fnBcrypt(newPassword, function (error, hash) {
-                            if (error) {
-                                self.render({
-                                    message: '密码加密出现错误',
-                                    failureInfo: error,
-                                });
-                            } else {
-                                // 更新数据库
-                                const $set = {
-                                    password: hash,
-                                    loginStamp: `${Math.random()}`.split('.')[1],
-                                };
-                                // 新用户名存在且新用户名不等于老用户名，才把老用户名更新为新用户名。
-                                if (newUsername !== '' && newUsername !== oldUsername) {
-                                    $set.username = newUsername;
-                                }
-                                Admins.update({_id: adminInfo._id}, {$set: $set}, function (error) {
-                                    // 数据库更新出现错误
-                                    if (error) {
-                                        self.render({
-                                            message: '密码更新出现错误',
-                                            failureInfo: error,
-                                        });
-                                        return;
-                                    }
-                                    delete session.adminInfo; // 不加这句话，改了密码，不会掉线，加了这句话也只是当前用户掉线，其他人不掉线，集体掉线需另做处理(数据库加loginStamp字段进行一系列处理)。
-                                    self.render({
-                                        status: 'success',
-                                        message: '已成功修改密码',
-                                    });
-                                });
-                            }
-                        });
-                    } else {
-                        self.render({
-                            message: '用户名和老密码不匹配',
-                        });
-                    }
-                });
-            });
-            // 新用户名存在且新用户名不等于老用户名，才去查询新用户名是否已经被注册。
-            if (newUsername !== '' && newUsername !== oldUsername) {
-                Admins.findOne({username: newUsername}, function (error, result) {
-                    if (error) {
-                        self.render({message: '数据库查询出现错误'});
-                    }
-                    if (result) {
-                        self.render({message: '新用户名已被他人使用'});
-                    } else {
-                        fnUpdateDB('select', {});
-                    }
-                });
-            }
             // 如果管理员账号存在并且老密码正确则可以修改密码
-            Admins.findOne({username: oldUsername}, function (error, result) {
+            Admins.findOne({username: adminInfo.username}, function (error, result) {
                 // 数据库查询出现错误
                 if (error) {
                     self.render({message: '数据库查询出现错误'});
                 }
                 if (result) {
-                    fnUpdateDB('update', result);
+                    result.comparePassword(oldPassword, function (error, isMatch) {
+                        if (error) {
+                            self.render({
+                                message: '密码对比出现错误',
+                                failureInfo: error,
+                            });
+                        }
+                        if (isMatch) {
+                            // 用户名和老密码匹配了,则进行密码替换
+                            const fnBcrypt = require('../../../../utils/bcrypt'); // 加密工具
+                            // 密码加密
+                            fnBcrypt(newPassword, function (error, hash) {
+                                if (error) {
+                                    self.render({
+                                        message: '密码加密出现错误',
+                                        failureInfo: error,
+                                    });
+                                } else {
+                                    // 更新数据库
+                                    const $set = {
+                                        password: hash,
+                                        loginStamp: `${Math.random()}`.split('.')[1],
+                                    };
+                                    Admins.update({_id: result._id}, {$set: $set}, function (error) {
+                                        // 数据库更新出现错误
+                                        if (error) {
+                                            self.render({
+                                                message: '密码更新出现错误',
+                                                failureInfo: error,
+                                            });
+                                            return;
+                                        }
+                                        delete session.adminInfo; // 不加这句话，改了密码，不会掉线，加了这句话也只是当前用户掉线，其他人不掉线，集体掉线需另做处理(数据库加loginStamp字段进行一系列处理)。
+                                        self.render({
+                                            status: 'success',
+                                            message: '已成功修改密码',
+                                        });
+                                    });
+                                }
+                            });
+                        } else {
+                            self.render({
+                                message: '用户名和老密码不匹配',
+                            });
+                        }
+                    });
                 } else {
                     self.render({message: '账号不存在'});
                 }
