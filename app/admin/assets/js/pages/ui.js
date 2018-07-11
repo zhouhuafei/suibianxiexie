8dcd54fc86c4b71e7082fb65538853be98a51750
zhouhuafei <zhouhuafei@shopex.cn>
2018-07-11 16:08:20 +0800

    md

--- app/admin/assets/js/pages/ui.js
+++ app/admin/assets/js/pages/ui.js
@@ -10,13 +10,74 @@ class Sub extends Super {
 
         // 验证
         (function () {
-            const ValidateInput = require('../components-dom/g-validate-form-hint');
-            const validateInput = new ValidateInput({element: '.js-validate-form'});
+            const validateInput = superSelf.validateInput;
             validateInput.setValidate('no-999', function (value) {
                 return Number(value) !== 999;
             });
-            document.querySelector('.js-button').addEventListener('click', function () {
-                console.log('isAllPassValidate', validateInput.isAllPassValidate());
+            document.querySelector('.js-save').addEventListener('click', function () {
+                // 测试确认框和提示框
+                const DialogAlert = require('../components-dom/g-dialog-alert');
+                const DialogConfirm = require('../components-dom/g-dialog-confirm');
+                new DialogConfirm({
+                    callback: {
+                        cancel: function () {
+                            new DialogAlert({
+                                config: {
+                                    time: 3000, // 展示的时间
+                                    isShowIcon: false, // 是否显示icon
+                                    isShowClose: true, // 是否显示关闭按钮
+                                    icon: 'icon-success', // icon的class
+                                    content: '已取消', // 内容信息
+                                    positionLocation: 'center', // 弹窗的定位位置    positionMethod定位方式强制fixed
+                                },
+                            });
+                        },
+                        confirm: function () {
+                            const isAllPassValidate = validateInput.isAllPassValidate();
+                            new DialogAlert({
+                                config: {
+                                    time: 3000, // 展示的时间
+                                    isShowIcon: false, // 是否显示icon
+                                    isShowClose: true, // 是否显示关闭按钮
+                                    icon: 'icon-success', // icon的class
+                                    content: isAllPassValidate ? '验证已通过，可执行保存操作' : '验证尚未通过，不可执行保存操作', // 内容信息
+                                    positionLocation: 'center', // 弹窗的定位位置    positionMethod定位方式强制fixed
+                                },
+                            });
+                        },
+                        close: function () {
+                            new DialogAlert({
+                                config: {
+                                    time: 3000, // 展示的时间
+                                    isShowIcon: false, // 是否显示icon
+                                    isShowClose: true, // 是否显示关闭按钮
+                                    icon: 'icon-success', // icon的class
+                                    content: '已关闭', // 内容信息
+                                    positionLocation: 'center', // 弹窗的定位位置    positionMethod定位方式强制fixed
+                                },
+                            });
+                        },
+                    },
+                    config: {
+                        positionLocation: 'center', // 弹窗的定位位置('top'，'center'，'bottom')。positionMethod定位方式强制fixed。
+                        isShowClose: true, // 是否显示关闭按钮
+                        closeContent: '<div class="iconfont icon-close"></div>', // 关闭按钮的内容
+                        isShowHeader: true, // 是否显示头部
+                        headerContent: '提示:', // 头部内容
+                        isShowBody: true, // 是否显示主体
+                        isShowIcon: false, // 是否显示icon
+                        icon: 'icon-warning', // icon的类型
+                        isCustom: false, // 是否自定义
+                        content: '<div>确定要执行这个操作?</div>', // 主体内容
+                        isShowFooter: true, // 是否显示尾部
+                        isShowConfirm: true, // 是否显示确认按钮
+                        confirmContent: '确认', // 确认按钮的内容
+                        isShowCancel: true, // 是否显示取消按钮
+                        cancelContent: '取消', // 取消按钮的内容
+                        isShowMask: true, // 是否显示遮罩
+                        isHandHide: false, // 是否手动隐藏(一般只用于点击确认时)
+                    },
+                });
             });
         }());
 
@@ -33,12 +94,11 @@ class Sub extends Super {
         ajax({
             url: dataInfo.api.list.route,
             method: 'post',
-            data: {type: 'ajax', obj: {test: false, key: 'obj', b: {a: 1}}, arr: ['a', 2, 'c', {a: 1}], arr2: []},
+            data: {type: 'ajax', obj: {test: true, key: 'obj', b: {a: 1}}, arr: ['a', 2, 'c', {a: 1}], arr2: []},
         }).then(function (json) {
             console.log('ajax测试application/x-www-form-urlencoded测试ajax:->', json);
         });
         // 测试multipart/form-data
-        /*
         const formData = new FormData();
         formData.append('test', 'test');
         axios({
@@ -54,69 +114,6 @@ class Sub extends Super {
             data: formData,
         }).then(function (json) {
             console.log('ajax测试multipart/form-data测试ajax:->', json);
-        });
-        */
-        // 测试确认框和提示框
-        const DialogAlert = require('../components-dom/g-dialog-alert');
-        const DialogConfirm = require('../components-dom/g-dialog-confirm');
-        new DialogConfirm({
-            callback: {
-                cancel: function () {
-                    new DialogAlert({
-                        config: {
-                            time: 3000, // 展示的时间
-                            isShowIcon: false, // 是否显示icon
-                            isShowClose: true, // 是否显示关闭按钮
-                            icon: 'icon-success', // icon的class
-                            content: '取消成功', // 内容信息
-                            positionLocation: 'center', // 弹窗的定位位置    positionMethod定位方式强制fixed
-                        },
-                    });
-                },
-                confirm: function () {
-                    new DialogAlert({
-                        config: {
-                            time: 3000, // 展示的时间
-                            isShowIcon: false, // 是否显示icon
-                            isShowClose: true, // 是否显示关闭按钮
-                            icon: 'icon-success', // icon的class
-                            content: '确认成功', // 内容信息
-                            positionLocation: 'center', // 弹窗的定位位置    positionMethod定位方式强制fixed
-                        },
-                    });
-                },
-                close: function () {
-                    new DialogAlert({
-                        config: {
-                            time: 3000, // 展示的时间
-                            isShowIcon: false, // 是否显示icon
-                            isShowClose: true, // 是否显示关闭按钮
-                            icon: 'icon-success', // icon的class
-                            content: '关闭成功', // 内容信息
-                            positionLocation: 'center', // 弹窗的定位位置    positionMethod定位方式强制fixed
-                        },
-                    });
-                },
-            },
-            config: {
-                positionLocation: 'center', // 弹窗的定位位置('top'，'center'，'bottom')。positionMethod定位方式强制fixed。
-                isShowClose: true, // 是否显示关闭按钮
-                closeContent: '<div class="iconfont icon-close"></div>', // 关闭按钮的内容
-                isShowHeader: true, // 是否显示头部
-                headerContent: '提示:', // 头部内容
-                isShowBody: true, // 是否显示主体
-                isShowIcon: false, // 是否显示icon
-                icon: 'icon-warning', // icon的类型
-                isCustom: false, // 是否自定义
-                content: '<div>确定要执行这个操作?</div>', // 主体内容
-                isShowFooter: true, // 是否显示尾部
-                isShowConfirm: true, // 是否显示确认按钮
-                confirmContent: '确认', // 确认按钮的内容
-                isShowCancel: true, // 是否显示取消按钮
-                cancelContent: '取消', // 取消按钮的内容
-                isShowMask: true, // 是否显示遮罩
-                isHandHide: false, // 是否手动隐藏(一般只用于点击确认时)
-            },
         });
     }
 }
