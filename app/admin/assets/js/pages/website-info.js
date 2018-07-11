2013c6c6552e322181193a65bfb83d3371df7647
zhouhuafei <zhouhuafei@shopex.cn>
2018-07-11 10:27:26 +0800

    md

--- app/admin/assets/js/pages/website-info.js
+++ app/admin/assets/js/pages/website-info.js
@@ -42,7 +42,6 @@ class Sub extends Super {
                     const h = result.height;
                     self.dataset.value = 'no-empty';
                     bg.style.backgroundImage = `url('${url}')`;
-                    text.innerText = `${w}*${h}`;
                     parent.querySelector('input[type=hidden]').value = resultStr;
                     parent.classList.add('g-upload_active');
                     superSelf.validateInput.validateInput(self);
