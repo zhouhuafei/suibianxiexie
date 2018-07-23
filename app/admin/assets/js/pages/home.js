ecf8b615a65a301797400b55b322c26cf8a9bfce
zhouhuafei <zhouhuafei@shopex.cn>
2018-07-23 11:04:19 +0800

    home

--- app/admin/assets/js/pages/home.js
+++ app/admin/assets/js/pages/home.js
@@ -2,6 +2,11 @@ require('../../scss/pages/home.scss');
 const Super = require('../pages-super/super');
 
 class Sub extends Super {
+    power() {
+        // 测试图片库
+        const Gallery = require('../components-dom-business/g-gallery');
+        new Gallery();
+    }
 }
 
 new Sub();
