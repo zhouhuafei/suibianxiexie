27a7c913e8344a91308dab50fc6c3dcc3234a20a
zhouhuafei <zhouhuafei@shopex.cn>
2018-11-02 10:42:38 +0800

    md

--- app/admin/controllers/pages/decorate-edit.js
+++ app/admin/controllers/pages/decorate-edit.js
@@ -1,6 +1,23 @@
 const Super = require('../pages-super/super'); // 超类型
 
 class Sub extends Super {
+    handleData() {
+        const self = this;
+        const opts = self.opts;
+        const req = opts.req;
+        const data = req.data;
+        const dataInfo = self.dataInfo;
+        if (data.type === 'custom') {
+            dataInfo.title = '自定义页面装修';
+        } else if (data.type === 'h5Home') {
+            dataInfo.title = 'h5首页装修';
+        } else if (data.type === 'wxHome') {
+            dataInfo.title = '微信小程序首页装修';
+        } else if (data.type === 'bdHome') {
+            dataInfo.title = '百度小程序首页装修';
+        }
+        self.render(); // 渲染视图(渲染数据)
+    }
 }
 
 module.exports = Sub;
