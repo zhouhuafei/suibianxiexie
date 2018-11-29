826cdc4d1a50844da1d3abcd3ff95c3a2d240199
zhouhuafei <1123486116@qq.com>
2018-11-29 23:30:12 +0800

    授权码

--- app-config.js
+++ app-config.js
@@ -11,7 +11,7 @@ class Config {
         this.isEnabledSingleDeviceLoginAdmin = true; // 后台管理系统是否启用单设备登录
         // 邮箱(短信)验证码，目前使用的qq邮箱进发送邮件，账号和授权码都是在qq邮箱申请的。
         this.verifyCodeRandomUser = 'this-is-a-code@foxmail.com'; // 发送者
-        this.verifyCodeRandomPass = 'paudrqaxbntmigfe'; // 授权码
+        this.verifyCodeRandomPass = 'oclyahdhdgepfjhb'; // 授权码
     }
 }
 
