5ada661968b24007ac0e80c49ad41a97d58abe00
zhouhuafei <1123486116@qq.com>
2018-12-26 23:28:16 +0800

    dist

--- dist/assets/static-cache/admin/js/pages/this-is-global-file-common.js
+++ dist/assets/static-cache/admin/js/pages/this-is-global-file-common.js
@@ -2615,9 +2615,7 @@ module.exports = function (json) {
         isHandleError: true, // 是否处理错误
         isHandleFailure: true, // 是否处理失败
         isHandleSuccess: false, // 是否处理成功
-        headers: {
-            Authorization: localStorage.getItem('token') || undefined
-        },
+        headers: {},
         /*
         优先级：错误回调>失败回调>成功回调>完成回调。
         调用方法时，要么用回调的方式走异步，要么用Promise的方式。
@@ -2632,6 +2630,10 @@ module.exports = function (json) {
         callbackComplete: function callbackComplete() {// 请求完成的回调
         }
     }, json);
+    var token = localStorage.getItem('token');
+    if (token) {
+        opts.headers.Authorization = localStorage.getItem('token');
+    }
     /*
     * javascript axios GET params
     * javascript axios POST/PUT/DELETE data
