f4a6b27f4d9a5cdc73b7e05ecc92b80d2c293993
zhouhuafei <1123486116@qq.com>
2019-02-14 18:40:01 +0800

    md

--- app/admin/controllers/api/password-modify.js
+++ app/admin/controllers/api/password-modify.js
@@ -11,18 +11,13 @@ class Sub extends Super {
         const session = req.session;
         const adminInfo = session.adminInfo;
         const data = req.data;
-        const username = (data['username'] || '').trim(); // 用户名
         const oldPassword = (data['old-password'] || '').trim(); // 旧密码 -> isEmpty方法内部去掉了首尾空格,不适用于验证密码是否为空
         const newPassword = (data['new-password'] || '').trim(); // 新密码 -> isEmpty方法内部去掉了首尾空格,不适用于验证密码是否为空
         const repeatNewPassword = (data['repeat-new-password'] || '').trim(); // 新密码二次确认 -> isEmpty方法内部去掉了首尾空格,不适用于验证密码是否为空
         const verifyCodeCanvas = (data['verify-code-canvas'] || '').trim(); // 验证码,图文随机
         const sessionVerifyCodeCanvasAdmin = session.verifyCodeCanvasAdmin; // 先保存一份验证码，留着下面做验证。
         delete session.verifyCodeCanvasAdmin; // 请求一次之后就清掉验证码，无论成功失败，都要让验证码无效。
-        if (checkStr.isEmpty(username)) {
-            self.render({message: '用户名不能为空'});
-        } else if (username !== adminInfo.username) {
-            self.render({message: '用户名错误'});
-        } else if (oldPassword === '') {
+        if (oldPassword === '') {
             self.render({message: '旧密码不能为空'});
         } else if (newPassword === '' || repeatNewPassword === '') {
             self.render({message: '新密码不能为空'});
@@ -39,7 +34,7 @@ class Sub extends Super {
         } else {
             const Admin = require('../../models/mongoose/admin');
             // 如果管理员账号存在并且老密码正确则可以修改密码
-            Admin.findOne({username: adminInfo.username}, function (error, result) {
+            Admin.findOne({_id: adminInfo._id}, function (error, result) {
                 // 数据库查询出现错误
                 if (error) {
                     self.render({message: '数据库查询出现错误'});
