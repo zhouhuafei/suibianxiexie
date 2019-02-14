974857358708bdd8e28208ea27df572dfadec8dc
zhouhuafei <1123486116@qq.com>
2019-02-14 13:17:58 +0800

    md

--- app/admin/assets/js/api/axios.js
+++ app/admin/assets/js/api/axios.js
@@ -4,6 +4,32 @@ const typeOf = require('zhf.type-of');
 const Message = require('zhf.g-ui/dist/js/components_dom/g-message/index.js');
 const qs = require('qs');
 
+/*
+// 添加请求拦截器
+axios.interceptors.request.use(function (config) {
+    // 在发送请求之前做些什么
+    // return config; // config是请求的配置参数。重点：此处如果返回Promise对象，可以阻塞接口请求。如此直接就可以解决刷新token时的接口并发问题。
+    // 以下是返回Promise对象的案例。
+    return new Promise(function (resolve, reject) {
+        setTimeout(function () { // 模拟等待refreshToken
+            resolve(config);
+        }, 5000);
+    });
+}, function (error) {
+    // 对请求错误做些什么
+    return Promise.reject(error);
+});
+// 添加响应拦截器
+axios.interceptors.response.use(function (response) {
+    // const config = response.config; // config是请求的配置参数
+    // 对响应数据做点什么
+    return response;
+}, function (error) {
+    // 对响应错误做点什么
+    return Promise.reject(error);
+});
+*/
+
 module.exports = function (json) {
     json.method = (json.method || json.type || 'GET').toUpperCase(); // 这里和$.ajax是不一样的，这里以前使用$.ajax的习惯传入type
     const opts = extend({
