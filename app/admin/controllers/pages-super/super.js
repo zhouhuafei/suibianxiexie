69ba169cd74b7efabd1397d7c7ea8898a6e0c8d2
zhouhuafei <1123486116@qq.com>
2019-09-27 00:16:36 +0800

    md

--- app/admin/controllers/pages-super/super.js
+++ app/admin/controllers/pages-super/super.js
@@ -236,7 +236,7 @@ class Super {
         const opts = self.opts;
         const req = opts.req;
         const data = req.data;
-        self.render(); // 渲染视图(渲染数据)。这个方法没必要放到handleData中调用吧。handleData只处理数据不就行了？待续...
+        self.render(); // 渲染视图(渲染数据)。这个方法没必要放到handleData中调用吧。handleData只处理数据不就行了？答：因为异步，所以需要手动调用。
     }
 
     // (渲)渲染视图
